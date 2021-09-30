580add15671f7f5c7a4c03c6ef4658a092d8c420
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-30 11:06:37 +0200

    Add TyreSpecificationRepository #19

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/TyreSpecificationRepository.java
@@ -0,0 +1,6 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface TyreSpecificationRepository extends MongoRepository<TyreSpecificationRepository, String> {
+}
