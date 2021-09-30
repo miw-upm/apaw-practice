167b9b14cc209aa39abd1aa6caa2751db7e93b3c
Mariavernia <maversa2@alumni.uv.es>
2021-09-30 11:03:28 +0200

    Fix cinema seeder #17

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/cinema/CinemaSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/cinema/CinemaSeederService.java
@@ -1,5 +1,8 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.cinema;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CinemaSeederService {
     public void seedDatabase() {
         //TO-DO
