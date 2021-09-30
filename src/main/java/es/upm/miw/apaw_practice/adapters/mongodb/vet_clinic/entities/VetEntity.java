434536293ea54b96af41053fa0c21b11444f4bb2
Mariu <100346843@alumnos.uc3m.es>
2021-09-30 12:30:30 +0200

    created entities and daos #21

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/vet_clinic/entities/VetEntity.java
@@ -0,0 +1,70 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.vet_clinic.entities;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+import java.util.UUID;
+
+@Document
+public class VetEntity {
+    @Id
+    private String id;
+    @DBRef
+    private List<AppointmentEntity> appointmentEntities;
+    @Indexed(unique = true)
+    private Integer vetNumber;
+    private String name;
+    private String surname;
+
+    public VetEntity(){
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<AppointmentEntity> getAppointmentEntities() {
+        return appointmentEntities;
+    }
+
+    public void setAppointmentEntities(List<AppointmentEntity> appointmentEntities) {
+        this.appointmentEntities = appointmentEntities;
+    }
+
+    public Integer getVetNumber() {
+        return vetNumber;
+    }
+
+    public void setVetNumber(Integer vetNumber) {
+        this.vetNumber = vetNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public int hashCode() {
+        return vetNumber.hashCode();
+    }
+}
