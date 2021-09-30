0cca28889261cdcfe78a37efd993d34330c5d1ba
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-30 12:43:57 +0200

    Add carWorkshopSeeder to DatabaseSeeder #19

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.CarWorkshopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,19 +9,23 @@ import org.springframework.stereotype.Service;
 public class DatabaseSeederService {
 
     private final ShopSeederService shopSeederService;
+    private final CarWorkshopSeederService carWorkshopSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, CarWorkshopSeederService carWorkshopSeederService) {
         this.shopSeederService = shopSeederService;
+        this.carWorkshopSeederService = carWorkshopSeederService;
         this.seedDatabase();
     }
 
     public void seedDatabase() {
         this.shopSeederService.seedDatabase();
+        this.carWorkshopSeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
+        this.carWorkshopSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
