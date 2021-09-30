945b295904b64e99eff379f03706b55860624a6d
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-30 12:30:06 +0200

    Fix inheritance #19

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/TyreSpecificationRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/TyreSpecificationRepository.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.TyreSpecificationEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TyreSpecificationRepository extends MongoRepository<TyreSpecificationRepository, String> {
+public interface TyreSpecificationRepository extends MongoRepository<TyreSpecificationEntity, String> {
 }
