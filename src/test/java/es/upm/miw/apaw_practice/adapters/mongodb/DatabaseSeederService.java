0b7eaaf544309ccdd1642cb19ddc1cc545953591
Mariavernia <maversa2@alumni.uv.es>
2021-09-30 10:48:49 +0200

    Add CinemaSeederService to DatabaseSeeder

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.cinema.CinemaSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.zoo.ZooSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,13 @@ public class DatabaseSeederService {
 
     private final ShopSeederService shopSeederService;
     private final ZooSeederService zooSeederService;
+    private final CinemaSeederService cinemaSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, ZooSeederService zooSeederService, CinemaSeederService cinemaSeederService) {
         this.shopSeederService = shopSeederService;
         this.zooSeederService = zooSeederService;
+        this.cinemaSeederService = cinemaSeederService;
         this.seedDatabase();
     }
 
