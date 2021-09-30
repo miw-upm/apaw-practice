8ebf07b765461fc5514cc1d75d0235d65a92db6e
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-09-30 12:25:52 +0200

    Add CarWorkshopSeederService #19

---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/CarWorkshopSeederService.java
@@ -0,0 +1,59 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.CarRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.TyreRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.TyreSpecificationRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.CarEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OwnerEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.TyreEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.TyreSpecificationEntity;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CarWorkshopSeederService {
+    @Autowired
+    private CarRepository carRepository;
+    @Autowired
+    private TyreRepository tyreRepository;
+    @Autowired
+    private TyreSpecificationRepository tyreSpecsRepository;
+
+    public void seedDatabase() {
+        LogManager.getLogger(this.getClass()).warn("-- Car Workshop Initial Load --");
+        TyreSpecificationEntity[] tyreSpecs = {
+                new TyreSpecificationEntity(205, 16, "100Y"),
+                new TyreSpecificationEntity(205, 16, "95Z"),
+                new TyreSpecificationEntity(225, 17, "95T"),
+                new TyreSpecificationEntity(215, 17, "90T")
+        };
+        this.tyreSpecsRepository.saveAll(Arrays.asList(tyreSpecs));
+        TyreEntity[] tyres = {
+                new TyreEntity("Hankook", "Ventus Prime", new BigDecimal("55.00"), tyreSpecs[0]),
+                new TyreEntity("Hankook", "Kinergy", new BigDecimal("79.99"), tyreSpecs[1]),
+                new TyreEntity("Michelin", "Primacy", new BigDecimal("109.00"), tyreSpecs[1]),
+                new TyreEntity("Bridgestone", "Turanza", new BigDecimal("150.00"), tyreSpecs[2])
+        };
+        this.tyreRepository.saveAll(Arrays.asList(tyres));
+        CarEntity[] cars = {
+                new CarEntity("1111AAA", true, new OwnerEntity("00000000Z", "John Doe"),
+                        List.of(tyreSpecs[0], tyreSpecs[1])),
+                new CarEntity("2222BBB", false, new OwnerEntity("00000000Z", "John Doe"),
+                        List.of(tyreSpecs[3])),
+                new CarEntity("3333CCC", false, new OwnerEntity("99999999A", "Jane Doe"),
+                        List.of(tyreSpecs[2], tyreSpecs[3]))
+        };
+        this.carRepository.saveAll(Arrays.asList(cars));
+    }
+
+    public void deleteAll() {
+        this.carRepository.deleteAll();
+        this.tyreRepository.deleteAll();
+        this.tyreSpecsRepository.deleteAll();
+    }
+}
