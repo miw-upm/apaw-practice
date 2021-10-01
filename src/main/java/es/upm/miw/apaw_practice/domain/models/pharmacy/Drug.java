3850ea803370a47c073d2d7c420828fb8585053b
yoliyu <yolanda.sabuco.93@gmail.com>
2021-10-01 10:48:39 +0200

    Added Pharmacy Model #29

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/pharmacy/Drug.java
@@ -0,0 +1,12 @@
+package es.upm.miw.apaw_practice.domain.models.pharmacy;
+
+import java.math.BigDecimal;
+
+public class Drug {
+
+    private String name;
+    private ActiveIngredient activeIngredient;
+    private Boolean commercialized;
+    private BigDecimal price;
+
+}
