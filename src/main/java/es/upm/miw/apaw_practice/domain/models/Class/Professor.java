7053a38ec7c406817e6153f5ba497924fc178e6f
sosossss <wenxiang.wang@alumnos.upm.es>
2021-10-03 11:52:25 +0200

    Add Professor #13

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/Class/Professor.java
@@ -0,0 +1,37 @@
+package es.upm.miw.apaw_practice.domain.models.Class;
+
+import java.time.LocalDate;
+
+public class Professor {
+
+    private String name;
+    private String course;
+    private int age;
+    private LocalDate EntryDate;
+
+    public Professor(){
+        //empty for framework
+    }
+
+    public Professor(String name,String course,int age,LocalDate EntryDate){
+        this.name = name;
+        this.course = course;
+        this.age = age;
+        this.EntryDate = EntryDate;
+    }
+
+    public String getName(){ return name;}
+    public void setName(String name){ this.name = name;}
+
+    public String getCourse(){ return course;}
+    public void setCourse(String course){ this.course = course;}
+
+    public int getAge(){ return age;}
+    public void setAge(int age){ this.age = age;}
+
+    public LocalDate getEntryData(){ return EntryDate;}
+    public void setEntryDate(LocalDate EntryData){ this.EntryDate = EntryData;}
+
+
+
+}
