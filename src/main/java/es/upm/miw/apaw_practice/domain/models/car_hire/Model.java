2f0993094965b668d10ca7fd979e6d67ecc60620
Jano Martín <jano.martin.lopez@alumnos.upm.es>
2021-10-04 16:38:04 +0200

    Updated UML & car_hire models #26

--- src/main/java/es/upm/miw/apaw_practice/domain/models/car_hire/Model.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/car_hire/Model.java
@@ -1,19 +1,24 @@
 package es.upm.miw.apaw_practice.domain.models.car_hire;
 
+import java.util.List;
+
 public class Model {
 
     private String type;
     private String description;
     private Integer enginePower;
 
+    private List<Vehicle> vehicles;
+
     public Model() {
         //empty for framework
     }
 
-    public Model(String type, String description, Integer enginePower) {
+    public Model(String type, String description, Integer enginePower, List<Vehicle> vehicles) {
         this.type = type;
         this.description = description;
         this.enginePower = enginePower;
+        this.setVehicles(vehicles);
     }
 
     public String getType() {
@@ -40,12 +45,21 @@ public class Model {
         this.enginePower = enginePower;
     }
 
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
     @Override
     public String toString() {
         return "Model{" +
                 "type='" + type + '\'' +
                 ", description='" + description + '\'' +
                 ", enginePower=" + enginePower +
+                ", vehicles=" + vehicles +
                 '}';
     }
 }
