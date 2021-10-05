cd0dffc943c453b220c32989822d1def9079bf54
Adrian <adrian.callejo@alumnos.upm.es>
2021-10-05 12:22:47 +0200

    Partial commit #54

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/hospital/persistence/DoctorPersistenceMongodb.java
@@ -0,0 +1,7 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.hospital.persistence;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.hospital.DoctorPersistence;
+
+public class DoctorPersistenceMongodb implements DoctorPersistence {
+
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/hospital/DoctorPersistence.java
@@ -0,0 +1,6 @@
+package es.upm.miw.apaw_practice.domain.persistence_ports.hospital;
+
+public interface DoctorPersistence {
+
+    String readNicks();
+}
