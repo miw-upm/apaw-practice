dca0eba817e0a150c59bc11b52742a91b434cfd8
Blanca <b.castano@almatech.es>
2021-10-05 19:05:42 +0200

    fix game-wow model #70

--- src/main/java/es/upm/miw/apaw_practice/domain/models/game_wow/Raid.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/game_wow/Raid.java
@@ -7,12 +7,60 @@ public class Raid {
     private Date date;
     private String name;
     private String dificulty;
-    private int playerNumber;
-    private boolean finish;
+    private Integer playerNumber;
+    private Boolean finish;
     private Boss boss;
 
     public Raid() {
         //empty for framework
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDificulty() {
+        return dificulty;
+    }
+
+    public void setDificulty(String dificulty) {
+        this.dificulty = dificulty;
+    }
+
+    public Integer getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(Integer playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public Boolean getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Boolean finish) {
+        this.finish = finish;
+    }
+
+    public Boss getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boss boss) {
+        this.boss = boss;
     }
 
     @Override
