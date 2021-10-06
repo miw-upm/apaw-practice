04a56e63ce4ba582f7456aac0c3e73e5afa91451
pierorodriguez09 <33253562+prodrang@users.noreply.github.com>
2021-10-06 19:33:59 +0200

    Added entities model #74

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/restaurant/entities/ClientEntity.java
@@ -0,0 +1,103 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.restaurant.entities;
+
+import es.upm.miw.apaw_practice.domain.models.restaurant.Client;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Document
+public class ClientEntity {
+    @Id
+    private String id;
+    private String name;
+    private String dni;
+    private LocalDate registrationDate;
+    @DBRef
+    private List<WaiterEntity> waiters;
+    @DBRef
+    private TableEntity table;
+
+    public ClientEntity(){
+        //empty for framework
+    }
+
+    public ClientEntity(Client client){
+        BeanUtils.copyProperties(client,this);
+        this.id = UUID.randomUUID().toString();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public List<WaiterEntity> getWaiters() {
+        return waiters;
+    }
+
+    public void setWaiters(List<WaiterEntity> waiters) {
+        this.waiters = waiters;
+    }
+
+    public TableEntity getTable() {
+        return table;
+    }
+
+    public void setTable(TableEntity table) {
+        this.table = table;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.id.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj || obj != null && getClass() == obj.getClass() && (id.equals(((ClientEntity) obj).id));
+    }
+
+    @Override
+    public String toString() {
+        return "ClientEntity{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", dni='" + dni + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", waiters=" + waiters +
+                ", table=" + table +
+                '}';
+    }
+}
