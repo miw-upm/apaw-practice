8f6b0c57c50794d5fa36f275997b4474a8e3a807
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-07 09:30:20 +0200

    Add findByDni #45

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/car_workshop/OwnerPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/car_workshop/OwnerPersistence.java
@@ -1,2 +1,7 @@
-package es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop;public interface OwnerPersistence {
+package es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop;
+
+import es.upm.miw.apaw_practice.domain.models.car_workshop.Owner;
+
+public interface OwnerPersistence {
+    Owner findByDni(String dni);
 }
