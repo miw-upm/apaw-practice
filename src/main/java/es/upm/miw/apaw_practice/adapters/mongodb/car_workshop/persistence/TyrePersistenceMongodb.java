7d8de50c0c1f431c2b92a1239095655101ccfe7d
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-07 10:55:50 +0200

    Initialise class #78

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/TyrePersistenceMongodb.java
@@ -0,0 +1,22 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.TyreRepository;
+import es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop.TyrePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("tyrePersistence")
+public class TyrePersistenceMongodb implements TyrePersistence {
+
+    private final TyreRepository tyreRepository;
+
+    @Autowired
+    public TyrePersistenceMongodb(TyreRepository tyreRepository) {
+        this.tyreRepository = tyreRepository;
+    }
+
+    @Override
+    public Integer deleteManufacturer(String manufacturer) {
+        return null;
+    }
+}
