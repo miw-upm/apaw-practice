afeb7f0e217ed0e952d3c4ffbd982f24914b1177
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-07 12:44:13 +0200

    Update read and create #78

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/TyrePersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/TyrePersistenceMongodb.java
@@ -1,6 +1,8 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.TyreRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.TyreEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.car_workshop.Tyre;
 import es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop.TyrePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +24,17 @@ public class TyrePersistenceMongodb implements TyrePersistence {
     }
 
     @Override
-    public Tyre read(String model){};
+    public Tyre read(String model){
+        return this.tyreRepository
+                .findByModel(model)
+                .orElseThrow(() -> new NotFoundException("Tyre model: " + model))
+                .toTyre();
+    };
 
     @Override
-    public void create(Tyre tyre){}
+    public void create(Tyre tyre){
+        this.tyreRepository
+                .save(new TyreEntity(tyre));
+    }
 
 }
