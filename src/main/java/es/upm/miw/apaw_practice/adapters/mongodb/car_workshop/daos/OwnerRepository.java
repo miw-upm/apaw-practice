978c15bcd29d613e917addeac8b8ac0711460ce5
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-07 09:41:36 +0200

    Fix extends #45

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OwnerRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OwnerRepository.java
@@ -1,9 +1,10 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OwnerEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface OwnerRepository {
+public interface OwnerRepository extends MongoRepository<OwnerEntity, String> {
     Optional<OwnerEntity> findByDni(String dni);
 }
