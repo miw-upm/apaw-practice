5f007c7b08ac1465f27f3effc889303e6c96a4a8
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-07 10:48:09 +0200

    Add @Repository #45

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/car_workshop/TyrePersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/car_workshop/TyrePersistence.java
@@ -1,5 +1,8 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface TyrePersistence {
     Integer deleteManufacturer(String manufacturer);
 }
