fc69c897d2ad634f7928e776916e83eeb8aa7e7d
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-07 09:30:53 +0200

    Add findByDni #45

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OwnerRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/daos/OwnerRepository.java
@@ -1,2 +1,7 @@
-package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;public interface OwnerRepository {
+package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos;
+
+import es.upm.miw.apaw_practice.domain.models.car_workshop.Owner;
+
+public interface OwnerRepository {
+    Owner findByDni(String dni);
 }
