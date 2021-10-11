659a5d37066b7d2e5e044613a00760eb503267a8
Jano Martín <jano.martin.lopez@alumnos.upm.es>
2021-10-11 21:17:55 +0200

    Added Get /car-hire/models/search #102

---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/car_hire/ModelResourceIT.java
@@ -0,0 +1,38 @@
+package es.upm.miw.apaw_practice.adapters.rest.car_hire;
+
+import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.car_hire.Model;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RestTestConfig
+public class ModelResourceIT {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Test
+    void testGet() {
+        this.webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(ModelResource.MODELS + ModelResource.SEARCH)
+                        .queryParam("q", "VIN_Number:VSSZZZ6KZ1R149943")
+                        .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Model.class)
+                .value(model -> assertEquals("Opel Insignia", model.getType()))
+                .value(model -> assertEquals("Tipo Berlina, manual", model.getDescription()))
+                .value(model -> assertEquals(140, model.getEnginePower()))
+                .value(model -> assertEquals(new BigDecimal("50"), model.getVehicleList().get(0).getDailyCost()))
+                .value(model -> assertEquals(25400, model.getVehicleList().get(0).getKilometersAmount()))
+                .value(model -> assertTrue(model.getVehicleList().get(0).getGoodCondition()));
+    }
+}
