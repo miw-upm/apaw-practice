5cb5d12d34dc21fc25897350b042735171ff8b7a
Alvaro Tango Fernandez <alvaro.tangofdez@gmail.com>
2021-10-11 20:21:08 +0200

    fixing code smells #97

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/zoo/ZooPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/zoo/ZooPersistence.java
@@ -1,10 +1,8 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.zoo;
 
-import es.upm.miw.apaw_practice.domain.models.zoo.Cage;
 import es.upm.miw.apaw_practice.domain.models.zoo.CageFumigation;
 import es.upm.miw.apaw_practice.domain.models.zoo.Zoo;
 import org.springframework.stereotype.Repository;
-import java.util.stream.Stream;
 
 @Repository
 public interface ZooPersistence {
