516ef21a3ffa2ba0d5a279ec0f92a77c64b1bf5a
Alvaro Tango Fernandez <alvaro.tangofdez@gmail.com>
2021-10-11 20:49:31 +0200

    get zoo addresses by cage location code #97

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/zoo/daos/CageRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/zoo/daos/CageRepository.java
@@ -3,12 +3,11 @@ package es.upm.miw.apaw_practice.adapters.mongodb.zoo.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.zoo.entities.CageEntity;
 import es.upm.miw.apaw_practice.adapters.mongodb.zoo.entities.ZooEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public interface CageRepository extends MongoRepository<CageEntity, String> {
 
-    Optional<CageEntity> findByLocationCode(String locationCode);
+    Stream<CageEntity> findByLocationCode(String locationCode);
 
     Stream<CageEntity> findByZoo(ZooEntity zooEntity);
 
