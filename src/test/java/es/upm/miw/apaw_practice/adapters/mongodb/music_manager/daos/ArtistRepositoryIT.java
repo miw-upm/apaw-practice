9a76946b3ffd5bdefc57551cf14e2f6982c0edc0
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-10-13 20:39:18 +0200

    Use firstName and familyName as identifiers for Artists Resource #100

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/music_manager/daos/ArtistRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/music_manager/daos/ArtistRepositoryIT.java
@@ -1,10 +1,11 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.music_manager.daos;
 
 import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.music_manager.entities.ArtistEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
 class ArtistRepositoryIT {
@@ -20,5 +21,15 @@ class ArtistRepositoryIT {
                                 artist.getId() != null &&
                                 artist.getFamilyName().equals("Grohl") &&
                                 artist.getAge().equals(52)));
+    }
+
+    @Test
+    void testByFirstNameAndFamilyName() {
+        assertTrue(this.artistRepository.findByFirstNameAndFamilyName("Paul", "McCartney").isPresent());
+        ArtistEntity artist = this.artistRepository.findByFirstNameAndFamilyName("Paul", "McCartney").get();
+        assertEquals("Paul", artist.getFirstName());
+        assertEquals("McCartney", artist.getFamilyName());
+        assertEquals(79, artist.getAge());
+        assertNotNull(artist.getId());
     }
 }
---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/music_manager/persistence/ArtistPersistenceMongodbIT.java
@@ -0,0 +1,45 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.music_manager.persistence;
+
+import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.models.music_manager.Artist;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestConfig
+class ArtistPersistenceMongodbIT {
+
+    @Autowired
+    private ArtistPersistenceMongodb artistPersistenceMongodb;
+
+    @Test
+    void testReadNotFound() {
+        assertThrows(NotFoundException.class, () -> this.artistPersistenceMongodb.readByFirstNameAndFamilyName("Aaa", "Bbbb"));
+    }
+
+    @Test
+    void testReadByFirstNameAndFamilyName() {
+        Artist artist = this.artistPersistenceMongodb.readByFirstNameAndFamilyName("Kurt", "Cobain");
+        assertEquals("Kurt", artist.getFirstName());
+        assertEquals("Cobain", artist.getFamilyName());
+        assertEquals(27, artist.getAge());
+    }
+
+    @Test
+    void testUpdate() {
+        Artist artist = this.artistPersistenceMongodb.readByFirstNameAndFamilyName("Dave", "Grohl");
+        assertEquals(52, artist.getAge());
+        artist.setAge(53);
+        artist = this.artistPersistenceMongodb.update(artist);
+        assertEquals("Dave", artist.getFirstName());
+        assertEquals("Grohl", artist.getFamilyName());
+        assertEquals(53, artist.getAge());
+        artist = this.artistPersistenceMongodb.readByFirstNameAndFamilyName("Dave", "Grohl");
+        assertEquals("Dave", artist.getFirstName());
+        assertEquals("Grohl", artist.getFamilyName());
+        assertEquals(53, artist.getAge());
+    }
+}
