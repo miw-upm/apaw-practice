f717bf36759e1202b430093a87c7b85f9d4c1810
Miguel Angel <miguelangelsanchezordaz@alumnos.upm.es>
2021-10-14 21:26:57 +0200

    Added PATCH /bands/ endpoint and tests #115

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/music_manager/persistence/BandPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/music_manager/persistence/BandPersistenceMongodb.java
@@ -1,10 +1,13 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.music_manager.persistence;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.music_manager.daos.ArtistRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.music_manager.daos.BandRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.music_manager.entities.ArtistEntity;
 import es.upm.miw.apaw_practice.adapters.mongodb.music_manager.entities.BandEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.music_manager.Band;
 import es.upm.miw.apaw_practice.domain.persistence_ports.music_manager.BandPersistence;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -28,6 +31,37 @@ public class BandPersistenceMongodb implements BandPersistence {
 
         return this.bandRepository
                 .save(new BandEntity(band.getBandName(), band.getOrigin(), band.getActive(), artistEntities))
+                .toBand();
+    }
+
+    @Override
+    public Band read(String bandName) {
+        return this.bandRepository
+                .findByBandName(bandName)
+                .orElseThrow(() -> new NotFoundException("Band bandName: " + bandName))
+                .toBand();
+    }
+
+    @Autowired
+    private ArtistRepository artistRepository;
+
+    @Override
+    public Band update(String bandName, Band band) {
+        BandEntity bandEntity = this.bandRepository
+                .findByBandName(bandName)
+                .orElseThrow(() -> new NotFoundException("Band bandName: " + bandName));
+
+        bandEntity.fromBand(band);
+        bandEntity.setArtistEntities(band.getArtists().stream()
+                .map(artist -> artistRepository
+                        .findByFirstNameAndFamilyName(artist.getFirstName(), artist.getFamilyName())
+                        .orElseThrow(() -> new NotFoundException("Artist fullName: " +
+                                artist.getFirstName() + " " + artist.getFamilyName()))
+                )
+                .collect(Collectors.toList()));
+
+        return bandRepository
+                .save(bandEntity)
                 .toBand();
     }
 }
