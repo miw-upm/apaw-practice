324c449effad4c0efc3b14e1f6c4ee6c3e4eabbb
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-15 16:33:57 +0200

    Add TyreSpecificationResource #121

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/car_workshop/TyreSpecificationService.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.domain.services.car_workshop;
+
+import es.upm.miw.apaw_practice.domain.models.car_workshop.TyreSpecsModification;
+
+public class TyreSpecificationService {
+    public void updateLoadSpeedIndex(TyreSpecsModification tyreSpecsModification) {
+    }
+}
