fb483afd24aab2b791c8a99afdc4d58d21284d7f
Víctor Claramunt <victor.claramuntv@alumnos.upm.es>
2021-10-16 02:51:10 +0200

    add ClassroomService test #39

---
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/university/ClassroomServiceIT.java
@@ -0,0 +1,26 @@
+package es.upm.miw.apaw_practice.domain.services.university;
+
+import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.persistence_ports.university.ClassroomPersistence;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestConfig
+public class ClassroomServiceIT {
+
+    @Autowired
+    private ClassroomService classroomService;
+
+    @Autowired
+    private ClassroomPersistence classroomPersistence;
+
+    @Test
+    void testDelete() {
+        this.classroomService.delete("ETSISI", 4302);
+        assertThrows(NotFoundException.class, () ->
+                this.classroomPersistence.readBySchoolAndNumber("ETSISI", 4302));
+    }
+}
