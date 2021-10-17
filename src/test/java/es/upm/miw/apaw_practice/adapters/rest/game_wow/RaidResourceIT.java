28212d9103e49093064f7d4feacf9d4deed6b0dc
Kara <blk20100@gmail.com>
2021-10-17 22:23:05 +0200

    Added PUT raidDificulty RaidResourceIT update test #122

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/game_wow/RaidResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/game_wow/RaidResourceIT.java
@@ -1,37 +1,37 @@
-package es.upm.miw.apaw_practice.adapters.mongodb.game_wow.persisitence;
+package es.upm.miw.apaw_practice.adapters.rest.game_wow;
 
-import es.upm.miw.apaw_practice.TestConfig;
-import es.upm.miw.apaw_practice.adapters.mongodb.game_wow.persistence.RaidPersistenceMongodb;
+import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
 import es.upm.miw.apaw_practice.domain.models.game_wow.Boss;
 import es.upm.miw.apaw_practice.domain.models.game_wow.Drop;
 import es.upm.miw.apaw_practice.domain.models.game_wow.Feature;
 import es.upm.miw.apaw_practice.domain.models.game_wow.Raid;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@TestConfig
-public class RaidPersistenceMongodbIT {
+@RestTestConfig
+public class RaidResourceIT {
 
     @Autowired
-    private RaidPersistenceMongodb raidPersistenceMongodb;
+    private WebTestClient webTestClient;
 
     @Test
-    void testUpdate () {
+    void testUpdate() {
         Date raidDate = new Date();
         Feature feature = new Feature("Legs", 171, 200, 100, "Use: Restores 1625 mana");
         Drop drop = new Drop("Plaguebringer's Stained Pants", "mage,priest,warlock", 264, feature);
         Boss boss = new Boss("Festergut", "25N", List.of(drop));
         Raid raidCreation = new Raid(raidDate, "ICC", "25N", 25, false, List.of(boss));
 
-        Raid raidBD = this.raidPersistenceMongodb.create(raidCreation);
-        raidBD.setDificulty("25H");
-        this.raidPersistenceMongodb.update(raidBD);
-        raidBD = this.raidPersistenceMongodb.readById(raidBD.getId());
-        assertEquals("25H", raidBD.getDificulty());
+        this.webTestClient
+                .put()
+                .uri(RaidResource.GAMEWOW_RAIDS + RaidResource.ID_ID + RaidResource.DIFICULTY, "asd")
+                .body(BodyInserters.fromValue("10H"))
+                .exchange()
+                .expectStatus().isNotFound();
     }
 }
