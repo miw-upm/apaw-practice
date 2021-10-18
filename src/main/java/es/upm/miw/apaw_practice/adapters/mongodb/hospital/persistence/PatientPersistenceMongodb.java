b38a9da6767e9fba7a345ddb1a730d7e0a518121
Adrian <adrian.callejo@alumnos.upm.es>
2021-10-18 13:04:14 +0200

    Finished PUT patient and test #56

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/hospital/persistence/PatientPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/hospital/persistence/PatientPersistenceMongodb.java
@@ -1,7 +1,11 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.hospital.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.hospital.daos.PatientRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.hospital.entities.PatientEntity;
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.models.hospital.Patient;
 import es.upm.miw.apaw_practice.domain.persistence_ports.hospital.PatientPersistence;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,5 +22,14 @@ public class PatientPersistenceMongodb implements PatientPersistence{
     @Override
     public void delete(String dni) {
         this.patientRepository.deleteByDni(dni);
+    }
+
+    @Override
+    public Patient update(String dni, Patient patient) {
+        PatientEntity patientEntity = this.patientRepository
+                .findByDni(patient.getDni()).get();
+
+        BeanUtils.copyProperties(patient, patientEntity, "id");
+        return this.patientRepository.save(patientEntity).toPatient();
     }
 }
