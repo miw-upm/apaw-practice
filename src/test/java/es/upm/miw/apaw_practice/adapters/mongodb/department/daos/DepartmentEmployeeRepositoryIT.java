c20a254be55cdec0f94f6b8d48e539a07e704fd5
Harold Lockward <HLockward12@gmail.com>
2021-10-19 01:05:54 +0200

    Added POST Department end-point #145

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/department/daos/DepartmentEmployeeRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/department/daos/DepartmentEmployeeRepositoryIT.java
@@ -1,11 +1,28 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.department.daos;
 
 import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.department.entities.DepartmentEmployeeEntity;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestConfig
 public class DepartmentEmployeeRepositoryIT {
 
     @Autowired
     private DepartmentEmployeeRepository departmentEmployeeRepository;
+
+    @Test
+    void testFindByDni() {
+        assertTrue(this.departmentEmployeeRepository.findByDni("00523821F").isPresent());
+        DepartmentEmployeeEntity departmentEmployeeEntity = this.departmentEmployeeRepository
+                .findByDni("00523821F")
+                .get();
+        assertEquals(false, departmentEmployeeEntity.getActive());
+        assertEquals(1984, departmentEmployeeEntity.getBirthday().getYear());
+        assertEquals(8, departmentEmployeeEntity.getBirthday().getMonth());
+        assertEquals(27, departmentEmployeeEntity.getBirthday().getDayOfMonth());
+    }
 }
