3d84c6d1ee943384fac76a6b4c1d79598fb87a3c
sosossss <wenxiang.wang@alumnos.upm.es>
2021-10-20 00:50:33 +0200

    Add Persistence #13

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Class/persistence/CoursePersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Class/persistence/CoursePersistenceMongodb.java
@@ -4,15 +4,17 @@ import es.upm.miw.apaw_practice.adapters.mongodb.Class.daos.CourseRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.Class.entities.CourseEntity;
 import es.upm.miw.apaw_practice.domain.persistence_ports.Class.CoursePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.stream.Stream;
 
-public class CoursePersistenceMobngodb implements CoursePersistence {
+@Repository("CoursePersistence")
+public class CoursePersistenceMongodb implements CoursePersistence {
 
     private final CourseRepository courseRepository;
 
     @Autowired
-    public CoursePersistenceMobngodb(CourseRepository courseRepository){
+    public CoursePersistenceMongodb(CourseRepository courseRepository){
         this.courseRepository = courseRepository;
     }
 
@@ -20,5 +22,10 @@ public class CoursePersistenceMobngodb implements CoursePersistence {
     public Stream<es.upm.miw.apaw_practice.domain.models.Class.Course> readAll() {
         return this.courseRepository.findAll().stream()
                 .map(CourseEntity::toCourse);
+    }
+
+    @Override
+    public void delete(String name){
+        this.courseRepository.deleteByName(name);
     }
 }
---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Class/persistence/ProfessorPersistenceMongodb.java
@@ -0,0 +1,26 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.Class.persistence;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.Class.daos.ProfessorRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.Class.entities.ProfessorEntity;
+import es.upm.miw.apaw_practice.domain.models.Class.Professor;
+import es.upm.miw.apaw_practice.domain.persistence_ports.Class.ProfessorPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
+
+@Repository("ProfessorPersistence")
+public class ProfessorPersistenceMongodb implements ProfessorPersistence {
+    private final ProfessorRepository professorRepository;
+
+    @Autowired
+    public ProfessorPersistenceMongodb(ProfessorRepository professorRepository){
+        this.professorRepository = professorRepository;
+    }
+
+    @Override
+    public Stream<Professor> readAll(){
+        return this.professorRepository.findAll().stream()
+                .map(ProfessorEntity::toProfessor);
+    }
+}
