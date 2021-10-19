e22ce1f4314fcb59f3e2489eaf443979aa93aa45
Raúl Calderón <raul.calderon@alumnos.upm.es>
2021-10-19 20:54:08 +0200

    Fix GET TvSeries EndPoint and Tests #185

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/tv_series/PlayerSeriesPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/tv_series/PlayerSeriesPersistence.java
@@ -1,9 +1,11 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.tv_series;
 
 import es.upm.miw.apaw_practice.domain.models.tv_series.Player;
+import org.springframework.stereotype.Repository;
 
 import java.util.stream.Stream;
 
+@Repository
 public interface PlayerSeriesPersistence {
 
     Stream<Player> readAll();
