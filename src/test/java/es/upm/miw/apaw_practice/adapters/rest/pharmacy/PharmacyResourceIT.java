7b41382de9e470c6cf4e303961486b708ff19201
yoliyu <yolanda.sabuco.93@gmail.com>
2021-10-20 23:41:37 +0200

    added PATCH endpoint #172 and tests

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/pharmacy/PharmacyResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/pharmacy/PharmacyResourceIT.java
@@ -3,10 +3,14 @@ package es.upm.miw.apaw_practice.adapters.rest.pharmacy;
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
 import es.upm.miw.apaw_practice.domain.models.pharmacy.Drug;
 import es.upm.miw.apaw_practice.domain.models.pharmacy.Pharmacy;
+import es.upm.miw.apaw_practice.domain.models.pharmacy.PharmacyDrugsUpdating;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestTestConfig
 public class PharmacyResourceIT {
@@ -22,5 +26,16 @@ public class PharmacyResourceIT {
                 .body(BodyInserters.fromValue(new Pharmacy()))
                 .exchange()
                 .expectStatus().isOk();
+    }
+
+    @Test
+    void testUpdateDrugs() {
+        List<Drug> drugs = List.of(new Drug("1234","Drug 1",true,new BigDecimal(2.30)));
+        this.webTestClient
+                .patch()
+                .uri(PharmacyResource.PHARMACIES)
+                .body(BodyInserters.fromValue(List.of(new PharmacyDrugsUpdating("1234", drugs))))
+                .exchange()
+                .expectStatus().isNotFound();
     }
 }
