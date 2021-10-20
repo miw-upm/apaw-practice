3e7e8f90be26801e2a1b7baa9bc70f83ea46df85
Mariu <100346843@alumnos.uc3m.es>
2021-10-20 13:28:42 +0200

    added put end-point & tests

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/vet_clinic/persistence/PetPersistenceMongodbIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/vet_clinic/persistence/PetPersistenceMongodbIT.java
@@ -2,9 +2,14 @@ package es.upm.miw.apaw_practice.adapters.mongodb.vet_clinic.persistence;
 
 import es.upm.miw.apaw_practice.TestConfig;
 import es.upm.miw.apaw_practice.adapters.mongodb.vet_clinic.daos.PetRepository;
+import es.upm.miw.apaw_practice.domain.models.vet_clinic.Diagnosis;
+import es.upm.miw.apaw_practice.domain.models.vet_clinic.Pet;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @TestConfig
@@ -23,5 +28,18 @@ public class PetPersistenceMongodbIT {
                         "Coca".equals(pet.getNick()) &&
                                 "Pedro".equals(pet.getOwner())
                 ));
+    }
+
+    @Test
+    void updateTest() {
+        List<Diagnosis> diagnosisList = List.of(
+                new Diagnosis("diagnosis1", "medicine1", false),
+                new Diagnosis("diagnosis2", "", false),
+                new Diagnosis("diagnosis5", "medicine5", true),
+                new Diagnosis("diagnosis6", "", false)
+        );
+        Pet pet = this.petPersistence.readByChip(2222);
+        Pet petUpdated = this.petPersistence.update(pet, diagnosisList);
+        assertEquals(diagnosisList, petUpdated.getDiagnosis());
     }
 }
