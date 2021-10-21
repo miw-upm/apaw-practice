b5452a14e87c33ca0a5e3790bbd8ca45fc811643
Jaime <jaimechacons@hotmail.com>
2021-10-21 21:28:05 +0200

    Added search GET /emarketer/emarketers/cups/{cup} #188

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/emarketer/EmarketerResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/emarketer/EmarketerResource.java
@@ -1,13 +1,17 @@
 package es.upm.miw.apaw_practice.adapters.rest.emarketer;
 
+import es.upm.miw.apaw_practice.domain.models.emarketer.Cups;
 import es.upm.miw.apaw_practice.domain.models.emarketer.Customer;
 import es.upm.miw.apaw_practice.domain.models.emarketer.Emarketer;
 import es.upm.miw.apaw_practice.domain.models.emarketer.Plan;
 import es.upm.miw.apaw_practice.domain.services.emarketer.EmarketerService;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
@@ -18,10 +22,10 @@ public class EmarketerResource {
     static final String EMARKETER = "/emarketer/emarketers";
 
     static final String NAME = "/{name}";
-
     static final String CUPS = "/cups";
-
     static final String CUP = "/{cup}";
+    static final String PLANS = "/plans";
+    static final String DESCRIPTION = "/{description}";
 
     private final EmarketerService emarketerService;
 
@@ -42,4 +46,11 @@ public class EmarketerResource {
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
+    @GetMapping(EmarketerResource.PLANS + EmarketerResource.DESCRIPTION)
+    public List<String> getDistinctCustomersNameListByEmarketerSystemic(@PathVariable String description) {
+        return this.emarketerService.getCustomersNameListByEmarketerSystemic(description)
+                .map(Customer::getName)
+                .distinct()
+                .collect(Collectors.toList());
+    }
 }
