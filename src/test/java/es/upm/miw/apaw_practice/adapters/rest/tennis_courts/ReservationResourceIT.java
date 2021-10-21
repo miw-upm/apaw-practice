8088d53579affd02d70959c9bb7f5b75e0331b91
aTorrecillas <a.torrecillas@alumnos.upm.es>
2021-10-21 13:51:41 +0200

    Update Reservation resource and entity test #61

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/tennis_courts/ReservationResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/tennis_courts/ReservationResourceIT.java
@@ -2,9 +2,14 @@ package es.upm.miw.apaw_practice.adapters.rest.tennis_courts;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.tennis_courts.Tennis_CourtsSeederService;
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.tennis_courts.Player;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RestTestConfig
 public class ReservationResourceIT {
@@ -28,5 +33,27 @@ public class ReservationResourceIT {
                 .expectStatus().isOk();
         this.tennis_courtsSeederService.deleteAll();
         this.tennis_courtsSeederService.seedDatabase();
+    }
+
+    @Test
+    void testUpdatePlayerList(){
+        Player[] players = {
+                new Player("00000006R"),
+                new Player("00000007R")
+        };
+        this.webTestClient.patch()
+                .uri(ReservationResource.RESERVATIONS + "/Pepe" + "/30:9:21" + "/13:00")
+                .body(BodyInserters.fromValue(players))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Player.class)
+                .value(playersDNIs -> assertEquals(4, playersDNIs.size()))
+                .value(playersDNIs -> assertEquals("00000003R", playersDNIs.get(0).getDNI()));
+        this.webTestClient.patch()
+                .uri(ReservationResource.RESERVATIONS + "/Sandra" + "/30:9:21" + "/13:00")
+                .body(BodyInserters.fromValue(players))
+                .exchange()
+                .expectStatus().isNotFound();
+
     }
 }
