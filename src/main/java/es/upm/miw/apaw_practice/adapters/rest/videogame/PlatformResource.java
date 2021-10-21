4d59cfc8795d68505586e74ac072479c832f3ade
mario irving <marioirving.morales@gmail.com>
2021-10-21 20:46:47 +0200

    feat POST #208

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame/PlatformResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame/PlatformResource.java
@@ -1,7 +1,10 @@
 package es.upm.miw.apaw_practice.adapters.rest.videogame;
 
+import es.upm.miw.apaw_practice.domain.models.videogame.Platform;
 import es.upm.miw.apaw_practice.domain.services.videogame.PlatformService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +19,9 @@ public class PlatformResource {
     @Autowired
     public PlatformResource(PlatformService platformService) { this.platformService = platformService; }
 
+    @PostMapping
+    public Platform create(@RequestBody Platform platform) {
+        platform.doDefault();
+        return this.platformService.create(platform);
+    }
 }
