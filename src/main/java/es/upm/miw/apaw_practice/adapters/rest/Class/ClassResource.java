4a656fdc2c9c1b2034ae74eac69791ca0d686bf6
sosossss <wenxiang.wang@alumnos.upm.es>
2021-10-22 13:41:33 +0200

    Add ClassResource #221

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/Class/ClassResource.java
@@ -0,0 +1,30 @@
+package es.upm.miw.apaw_practice.adapters.rest.Class;
+
+
+import es.upm.miw.apaw_practice.domain.models.Class.Class;
+import es.upm.miw.apaw_practice.domain.services.Class.ClassService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping(ClassResource.theClass)
+public class ClassResource {
+
+    static final String theClass = "/class";
+
+    private final ClassService classService;
+
+    @Autowired
+    public ClassResource(ClassService classService) {
+        this.classService = classService;
+    }
+
+    @GetMapping
+    public Stream<Class> readAll() {
+        return this.classService.readAll();
+    }
+}
