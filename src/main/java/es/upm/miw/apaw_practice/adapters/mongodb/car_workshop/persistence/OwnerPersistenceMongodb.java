bd3c0b33eb02cdde0c1af8fd68e52432d5cb8eb4
MarinaSayols <marina.sayols@alumnos.upm.es>
2021-10-23 13:35:32 +0200

    Add second search #160

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/OwnerPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/OwnerPersistenceMongodb.java
@@ -3,10 +3,13 @@ package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.persistence;
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.OwnerRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OwnerEntity;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw_practice.domain.models.car_workshop.Car;
 import es.upm.miw.apaw_practice.domain.models.car_workshop.Owner;
 import es.upm.miw.apaw_practice.domain.persistence_ports.car_workshop.OwnerPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
 
 @Repository("ownerPersistence")
 public class OwnerPersistenceMongodb implements OwnerPersistence {
@@ -29,6 +32,13 @@ public class OwnerPersistenceMongodb implements OwnerPersistence {
     public OwnerEntity findByName(String name) {
         return this.ownerRepository.findByName(name)
                 .orElseThrow(() -> new NotFoundException("Owner name: " + name));
+    }
+
+    @Override
+    public Stream<String> getDniFromCars(Stream<Car> cars) {
+        return cars.map(Car::getOwner)
+                .distinct()
+                .map(Owner::getDni);
     }
 
 }
--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/CarPersistenceMongodbIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/car_workshop/persistence/CarPersistenceMongodbIT.java
@@ -3,13 +3,16 @@ package es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.persistence;
 import es.upm.miw.apaw_practice.TestConfig;
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.daos.OwnerRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.OwnerEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.car_workshop.entities.TyreSpecificationEntity;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.car_workshop.Car;
+import es.upm.miw.apaw_practice.domain.models.car_workshop.TyreSpecification;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,5 +40,19 @@ public class CarPersistenceMongodbIT {
         List<Car> cars = this.carPersistence.findByOwnerAndRevision(owner, true).collect(Collectors.toList());
         assertEquals(1, cars.size());
         assertEquals("1111AAA", cars.get(0).getLicensePlate());
+    }
+
+    @Test
+    void testFindByTyreSpecifications() {
+        Stream<TyreSpecification> tyreSpecs = Stream.of(
+                new TyreSpecificationEntity(215, 17, "90T")
+                        .toTyreSpecification()
+        );
+        List<Car> cars = this.carPersistence.findByTyreSpecifications(tyreSpecs)
+                .collect(Collectors.toList());
+        assertEquals(2, cars.size());
+        List<String> licensePlates = cars.stream().map(Car::getLicensePlate)
+                .collect(Collectors.toList());
+        assertTrue(licensePlates.containsAll(List.of("2222BBB", "3333CCC")));
     }
 }
