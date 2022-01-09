7f2a6b44fb08bdcec2982cc5309c022799a8ff9f
zhengrongwang <736026544@qq.com>
2022-01-09 14:17:01 +0100

    Add DELETE course & Test #249

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/training/persistence/CoursePersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/training/persistence/CoursePersistenceMongodb.java
@@ -1,8 +1,21 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.training.persistence;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.training.daos.CourseRepository;
 import es.upm.miw.apaw_practice.domain.persistence_ports.training.CoursePersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("CoursePersistence")
 public class CoursePersistenceMongodb implements CoursePersistence {
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    public CoursePersistenceMongodb(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Override
+    public void deleteById(String id) {
+        this.courseRepository.deleteById(id);
+    }
 }
