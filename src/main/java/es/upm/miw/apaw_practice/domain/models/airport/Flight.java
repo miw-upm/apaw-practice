f15794663b3183314d06cf5b6d7f5d587cb10a28
ybrooks08 <yosbel88@gmail.com>
2022-01-15 16:35:15 +0100

    Model fixes. issue #260

--- src/main/java/es/upm/miw/apaw_practice/domain/models/airport/Flight.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/airport/Flight.java
@@ -1,20 +1,27 @@
 package es.upm.miw.apaw_practice.domain.models.airport;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Flight {
     private String number;
     private LocalDateTime date;
     private Boolean isCancelled;
+    private Airplane airplane;
+    private List<Pilot> pilots;
+    private List<Passenger> passengers;
 
     public Flight() {
         // empty for framework
     }
 
-    public Flight(String number, LocalDateTime date, String surname, Boolean isCancelled) {
+    public Flight(String number, LocalDateTime date, String surname, Boolean isCancelled, Airplane airplane, List<Pilot> pilots, List<Passenger> passengers) {
         this.number = number;
         this.date = date;
         this.isCancelled = isCancelled;
+        this.airplane = airplane;
+        this.pilots = pilots;
+        this.passengers = passengers;
     }
 
     public String getNumber() {
@@ -41,12 +48,39 @@ public class Flight {
         isCancelled = cancelled;
     }
 
+    public Airplane getAirplane() {
+        return airplane;
+    }
+
+    public void setAirplane(Airplane airplane) {
+        this.airplane = airplane;
+    }
+
+    public List<Pilot> getPilots() {
+        return pilots;
+    }
+
+    public void setPilots(List<Pilot> pilots) {
+        this.pilots = pilots;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(List<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
     @Override
     public String toString() {
         return "Flight{" +
                 "number='" + this.number + '\'' +
                 ", date='" + this.date + '\'' +
                 ", isCancelled=" + this.isCancelled +
+                ", airplane=" + this.airplane.toString() +
+                ", pilots=" + this.pilots.toString() +
+                ", passengers=" + this.passengers.toString() +
                 '}';
     }
 }
