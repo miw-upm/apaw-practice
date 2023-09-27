02b46ef36fd9cfb17733c48e7fbd211df1bb7553
JBernal <j.bernal@upm.es>
2023-09-27 22:42:04 +0200

    updated  CSRF from spring-boot 3.*

--- src/main/java/es/upm/miw/apaw_practice/SecurityConfiguration.java
+++ src/main/java/es/upm/miw/apaw_practice/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @Configuration
 @EnableReactiveMethodSecurity
@@ -16,6 +17,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
+                .csrf(csrf -> csrf.disable())
                 .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .build();
     }
