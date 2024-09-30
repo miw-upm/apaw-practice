dcca586c0549aff9825b9775e10964675f2c350c
Julián Sánchez <julian.sanchezfe@alumnos.upm.es>
2024-09-30 15:25:58 +0200

    Improves model by adding identification attributes to Degree ('code' i.e. '6100'), Student (email) and University ('topDomain', i.e. 'upm.es'). Also renames attribute 'maxStudents' of Degree to 'capacity' to improve readability #820

--- src/main/java/es/upm/miw/apaw_practice/domain/models/university/University.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/university/University.java
@@ -3,6 +3,7 @@ package es.upm.miw.apaw_practice.domain.models.university;
 import java.util.List;
 
 public class University {
+    private String topDomain;
     private String name;
     private boolean allowsInternationalStudents;
     private int numberOfFaculties;
@@ -12,11 +13,20 @@ public class University {
         //empty for framework
     }
 
-    public University(String name, boolean allowsInternationalStudents, int numberOfFaculties, List<Degree> degreesOffered) {
+    public University(String topDomain, String name, boolean allowsInternationalStudents, int numberOfFaculties, List<Degree> degreesOffered) {
+        this.topDomain = topDomain;
         this.name = name;
         this.allowsInternationalStudents = allowsInternationalStudents;
         this.numberOfFaculties = numberOfFaculties;
         this.degreesOffered = degreesOffered;
+    }
+
+    public String getTopDomain() {
+        return topDomain;
+    }
+
+    public void setTopDomain(String topDomain) {
+        this.topDomain = topDomain;
     }
 
     public String getName() {
@@ -54,7 +64,8 @@ public class University {
     @Override
     public String toString() {
         return "University{" +
-                "name='" + name + '\'' +
+                "topDomain='" + topDomain + '\'' +
+                ", name='" + name + '\'' +
                 ", allowsInternationalStudents=" + allowsInternationalStudents +
                 ", numberOfFaculties=" + numberOfFaculties +
                 ", degreesOffered=" + degreesOffered +
