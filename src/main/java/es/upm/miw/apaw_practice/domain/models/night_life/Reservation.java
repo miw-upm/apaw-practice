8e6d66ce3712ced7ff656fa6397bb13f7970bd7d
chanocb <101331282+chanocb@users.noreply.github.com>
2024-10-02 11:11:03 +0200

    Added Reservation #819

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/night_life/Reservation.java
@@ -0,0 +1,44 @@
+package es.upm.miw.apaw_practice.domain.models.night_life;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+    public LocalDate date;
+    public BigDecimal price;
+    public int numberOfPeople;
+
+    public Reservation(LocalDate date, BigDecimal price, int numberOfPeople) {
+        this.date = date;
+        this.price = price;
+        this.numberOfPeople = numberOfPeople;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public BigDecimal getPrice() {
+        return price;
+    }
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+                "date=" + date +
+                ", price=" + price +
+                ", numberOfPeople=" + numberOfPeople +
+                '}';
+    }
+}
