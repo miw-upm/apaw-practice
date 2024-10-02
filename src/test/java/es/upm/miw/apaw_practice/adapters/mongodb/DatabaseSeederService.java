e585fdbf8bc0e065d828d354917d1f16b5409bf4
Julián Sánchez <julian.sanchezfe@alumnos.upm.es>
2024-10-02 19:34:14 +0200

    Adds UniversitySeederService and integrates it on DatabaseSeederService #836

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.UniversitySeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,19 +9,23 @@ import org.springframework.stereotype.Service;
 public class DatabaseSeederService {
 
     private final ShopSeederService shopSeederService;
+    private final UniversitySeederService universitySeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, UniversitySeederService universitySeederService) {
         this.shopSeederService = shopSeederService;
+        this.universitySeederService = universitySeederService;
         this.seedDatabase();
     }
 
     public void seedDatabase() {
         this.shopSeederService.seedDatabase();
+        this.universitySeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
+        this.universitySeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/university/UniversitySeederService.java
@@ -0,0 +1,88 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.university;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.university.daos.DegreeRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.daos.StudentRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.daos.TeacherRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.daos.UniversityRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.entities.DegreeEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.entities.StudentEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.entities.TeacherEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.university.entities.UniversityEntity;
+import es.upm.miw.apaw_practice.domain.models.university.Degree;
+import es.upm.miw.apaw_practice.domain.models.university.Student;
+import es.upm.miw.apaw_practice.domain.models.university.Teacher;
+import es.upm.miw.apaw_practice.domain.models.university.University;
+import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class UniversitySeederService {
+
+    @Autowired
+    private DegreeRepository degreeRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private TeacherRepository teacherRepository;
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    public void seedDatabase() {
+        LogManager.getLogger(this.getClass()).warn("------- University Initial Load -----------");
+        DegreeEntity[] degrees = {
+                new DegreeEntity(new Degree(2000, 30, "Fine Arts", "Program that explores artistic expression in painting, sculpture and digital media, developing creative skills and techniques for innovative visual projects.")),
+                new DegreeEntity(new Degree(2001, 40, "Visual Arts", "Training focused on the analysis and creation of contemporary works, combining theory and practice in disciplines such as photography, video and graphic design.")),
+                new DegreeEntity(new Degree(2002, 45, "Sociology", "Study of social dynamics, structures and human behavior, providing tools to analyze phenomena such as inequality, culture and social change.")),
+                new DegreeEntity(new Degree(2003, 20, "Political Science", "Program that addresses the functioning of political systems, the theory of power and governance, preparing professionals to analyze and participate in public policies and electoral processes.")),
+                new DegreeEntity(new Degree(2004, 15, "Political Science", "Training in political theory, government systems and international relations, focused on understanding power, institutions and decision-making at global and local levels.")),
+        };
+        degreeRepository.saveAll(Arrays.asList(degrees));
+        StudentEntity[] students = {
+                new StudentEntity(new Student("emily.johnson@example.org", "Emily", "London", LocalDate.of(2004, 4, 12),
+                        new ArrayList<>())),
+                new StudentEntity(new Student("james.miller@example.org", "James", "Manchester", LocalDate.of(1997, 4, 17),
+                        List.of(degrees[1].toDegree()))),
+                new StudentEntity(new Student("sophia.davis@example.org", "Sophia", "Birmingham", LocalDate.of(2002, 6, 3),
+                        List.of(degrees[0].toDegree(), degrees[2].toDegree(), degrees[4].toDegree()))),
+                new StudentEntity(new Student("michael.brown@example.org", "Michael", "Liverpool", LocalDate.of(1989, 1, 29),
+                        List.of(degrees[1].toDegree(), degrees[2].toDegree()))),
+                new StudentEntity(new Student("olivia.wilson@example.org", "Olivia", "Liverpool", LocalDate.of(1999, 9, 12),
+                        List.of(degrees[0].toDegree())))
+        };
+        studentRepository.saveAll(Arrays.asList(students));
+        UniversityEntity[] universities = {
+                new UniversityEntity(new University("ox.ac.uk", "University of Oxford", true, 1,
+                        new ArrayList<>())),
+                new UniversityEntity(new University("cam.ac.uk", "University of Cambridge", false, 3,
+                        List.of(degrees[1].toDegree(), degrees[2].toDegree(), degrees[3].toDegree()))),
+                new UniversityEntity(new University("imperial.ac.uk", "Imperial College London", false, 7,
+                        List.of(degrees[0].toDegree(), degrees[2].toDegree()))),
+                new UniversityEntity(new University("manchester.ac.uk", "University of Manchester", true, 2,
+                        List.of(degrees[3].toDegree()))),
+                new UniversityEntity(new University("lse.ac.uk", "London School of Economics and Political Science", true, 3,
+                        List.of(degrees[0].toDegree(), degrees[1].toDegree())))
+        };
+        universityRepository.saveAll(Arrays.asList(universities));
+        TeacherEntity[] teachers = {
+                new TeacherEntity(new Teacher(null, "Carter", LocalDate.of(1980, 6, 12), "QQ123456A")),
+                new TeacherEntity(new Teacher(universities[0].toUniversity(), "Anderson", LocalDate.of(1976, 1, 2), "AB987654C")),
+                new TeacherEntity(new Teacher(universities[2].toUniversity(), "Matthews", LocalDate.of(1976, 6, 26), "CD876543B")),
+                new TeacherEntity(new Teacher(universities[3].toUniversity(), "Thompson", LocalDate.of(1991, 3, 23), "EF654321D")),
+                new TeacherEntity(new Teacher(universities[4].toUniversity(), "Roberts", LocalDate.of(1984, 9, 2), "GH345678E"))
+        };
+        teacherRepository.saveAll(Arrays.asList(teachers));
+    }
+
+    public void deleteAll() {
+        degreeRepository.deleteAll();
+        studentRepository.deleteAll();
+        teacherRepository.deleteAll();
+        universityRepository.deleteAll();
+    }
+}
