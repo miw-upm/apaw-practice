4d97c3201dc3bbfb797d3927c2dfdf01edd3b723
TrueAlexCv <alexcvtrue24282@gmail.com>
2024-10-03 12:50:25 +0200

    Added shopping-center classes and doc #834

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/shopping_center/Shop.java
@@ -0,0 +1,74 @@
+package es.upm.miw.apaw_practice.domain.models.shopping_center;
+
+import java.util.List;
+
+public class Shop {
+    private String id;
+    private String name;
+    private String address;
+    private List<Employee> employees;
+    private List<Provider> providers;
+
+    public Shop() {
+        //empty for framework
+    }
+
+    public Shop(String id, String name, String address, List<Employee> employees, List<Provider> providers) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.employees = employees;
+        this.providers = providers;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Provider> getProviders() {
+        return providers;
+    }
+
+    public void setProviders(List<Provider> providers) {
+        this.providers = providers;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", employees=" + employees +
+                ", providers=" + providers +
+                '}';
+    }
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/shopping_center/Ticket.java
@@ -0,0 +1,75 @@
+package es.upm.miw.apaw_practice.domain.models.shopping_center;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Ticket {
+    private String id;
+    private BigDecimal totalPrice;
+    private boolean isPaidByCreditCard;
+    private LocalDateTime date;
+    private Employee employee;
+
+    public Ticket() {
+        //empty for framework
+    }
+
+    public Ticket(String id, BigDecimal totalPrice, boolean isPaidByCreditCard, LocalDateTime date, Employee employee) {
+        this.id = id;
+        this.totalPrice = totalPrice;
+        this.isPaidByCreditCard = isPaidByCreditCard;
+        this.date = date;
+        this.employee = employee;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean isPaidByCreditCard() {
+        return isPaidByCreditCard;
+    }
+
+    public void setPaidByCreditCard(boolean paidByCreditCard) {
+        isPaidByCreditCard = paidByCreditCard;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id='" + id + '\'' +
+                ", totalPrice=" + totalPrice +
+                ", isPaidByCreditCard=" + isPaidByCreditCard +
+                ", date=" + date +
+                ", employee=" + employee +
+                '}';
+    }
+}
