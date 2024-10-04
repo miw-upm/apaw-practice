328c536259d09f0e690fd97018cdfe73b013c193
Manu Calvo <manucalvom@usal.es>
2024-10-05 01:24:49 +0200

    Fix missing import #846

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/military/MilitarySeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/military/MilitarySeederService.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 @Service
