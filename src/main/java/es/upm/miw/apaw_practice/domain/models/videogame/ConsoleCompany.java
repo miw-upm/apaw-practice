ab6bfc4dad843ae73a6ce945223893a8bf99c947
LuisLoboR16 <Lwolf95@hotmail.com>
2024-10-05 02:58:46 -0500

    Fixed classes and UML #826

--- src/main/java/es/upm/miw/apaw_practice/domain/models/videogame/ConsoleCompany.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/videogame/ConsoleCompany.java
@@ -1,41 +1,58 @@
 package es.upm.miw.apaw_practice.domain.models.videogame;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConsoleCompany {
-    private String name;
-    private String headquarter;
+    private String consoleCompany;
     private String website;
+    private int numberOfEmployees;
+    private boolean active;
+    private LocalDate foundationDate;
     private List<Console> consoles;
 
     private  ConsoleCompany(){
         //empty for framework
     }
 
-    public ConsoleCompany(String name, String headquarter, String website, List<Console> consoles) {
-        this.name = name;
-        this.headquarter = headquarter;
+    public ConsoleCompany(String consoleCompany,  String website, int numberOfEmployees, boolean active, LocalDate foundationDate, List<Console> consoles) {
+        this.consoleCompany = consoleCompany;
         this.website = website;
+        this.numberOfEmployees = numberOfEmployees;
+        this.active = active;
+        this.foundationDate = foundationDate;
         this.consoles = consoles;
     }
-    public String getName() {
-        return name;
+    public String getConsoleCompany() {
+        return consoleCompany;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getHeadquarter() {
-        return headquarter;
-    }
-    public void setHeadquarter(String headquarter) {
-        this.headquarter = headquarter;
+    public void setConsoleCompany(String consoleCompany) {
+        this.consoleCompany = consoleCompany;
     }
     public String getWebsite() {
         return website;
     }
     public void setWebsite(String website) {
         this.website = website;
+    }
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    public LocalDate getFoundationDate() {
+        return foundationDate;
+    }
+    public void setFoundationDate(LocalDate foundationDate) {
+        this.foundationDate = foundationDate;
     }
     public List<Console> getConsoles() {
         return consoles;
@@ -50,9 +67,11 @@ public class ConsoleCompany {
     @Override
     public String toString() {
         return "ConsoleCompany{" +
-                "name='" + name + '\'' +
-                ", headquarter=" + headquarter +
+                "consoleCompany='" + consoleCompany + '\'' +
                 ", website=" + website +
+                ", numberOfEmployees=" + numberOfEmployees +
+                ", active=" + active +
+                ", foundationDate=" + foundationDate +
                 ", consoles=" + consoles +
                 '}';
     }
