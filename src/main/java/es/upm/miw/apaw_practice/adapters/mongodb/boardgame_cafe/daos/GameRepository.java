021fb5537efd14b051555aa6578c149d01c5d5b4
joralvmel <joralvmel@gmail.com>
2024-10-05 19:42:00 +0200

    add boardgame-cafe DAOs #848

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/boardgame_cafe/daos/GameRepository.java
@@ -0,0 +1,10 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.boardgame_cafe.daos;
+
+import es.upm.miw.apaw_practice.adapters.mongodb.boardgame_cafe.entities.GameEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface GameRepository extends MongoRepository<GameEntity, String> {
+    Optional<GameEntity> findByGameName(String gameName);
+}
