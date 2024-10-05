1593211277a44fc19474748d3081619a474a0f73
Adrian Alonso <Adrianalonso2001@gmail.com>
2024-10-05 20:24:20 +0200

    leaf classes #855

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/wuhshu_sport/CompetitionForm.java
@@ -0,0 +1,53 @@
+package es.upm.miw.apaw_practice.domain.models.wuhshu_sport;
+
+import java.time.Duration;
+
+public class CompetitionForm {
+
+    Double score;
+    Duration duration;
+    String category;
+
+    public CompetitionForm() {
+        //empty for framework
+    }
+
+    public CompetitionForm(Double score, Duration duration, String category) {
+        this.score = score;
+        this.duration = duration;
+        this.category = category;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "CompetitionForm{" +
+                "score=" + score +
+                ", duration=" + duration +
+                ", category='" + category + '\'' +
+                '}';
+    }
+}
