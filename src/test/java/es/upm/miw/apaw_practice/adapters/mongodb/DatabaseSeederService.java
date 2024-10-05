ea7ad5871edf11b902d1ae233543b93ae6f746ee
laura <contact0faztweb.com>
2024-10-05 09:14:08 +0200

    added seeder service #841

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/DatabaseSeederService.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw_practice.adapters.mongodb;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.competition.CompetitionSeederService;
+import es.upm.miw.apaw_practice.adapters.mongodb.art_museum.ArtMuseumSeederService;
 import es.upm.miw.apaw_practice.adapters.mongodb.shop.ShopSeederService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,22 +11,27 @@ public class DatabaseSeederService {
 
     private final ShopSeederService shopSeederService;
     private final CompetitionSeederService competitionSeederService;
+    private final ArtMuseumSeederService artMuseumSeederService;
 
     @Autowired
-    public DatabaseSeederService(ShopSeederService shopSeederService, CompetitionSeederService competitionSeederService) {
+    public DatabaseSeederService(ShopSeederService shopSeederService, CompetitionSeederService competitionSeederService,
+                                 ArtMuseumSeederService artMuseumSeederService) {
         this.shopSeederService = shopSeederService;
         this.competitionSeederService = competitionSeederService;
+        this.artMuseumSeederService = artMuseumSeederService;
         this.seedDatabase();
     }
 
     public void seedDatabase() {
         this.shopSeederService.seedDatabase();
         this.competitionSeederService.seedDatabase();
+        this.artMuseumSeederService.seedDatabase();
     }
 
     public void deleteAll() {
         this.shopSeederService.deleteAll();
         this.competitionSeederService.deleteAll();
+        this.artMuseumSeederService.deleteAll();
     }
 
     public void reSeedDatabase() {
