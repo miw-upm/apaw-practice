7aac42e066cf72384cbdbc0ea892d5bfb958f7a7
sergiobq0123 <s.sanchezp@alumnos.upm.es>
2024-10-05 19:16:07 +0200

    Fix sonarcloud #853

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/competition/persistence/PlayerTeamPersistenceMongodbIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/competition/persistence/PlayerTeamPersistenceMongodbIT.java
@@ -23,6 +23,7 @@ class PlayerTeamPersistenceMongodbIT {
     void testDelete() {
         List<PlayerTeam> playerTeamList = this.playerTeamPersistenceMongodb.readAll().toList();
         int playerTeamListSize = playerTeamList.size();
+        assertEquals(playerTeamListSize, this.playerTeamPersistenceMongodb.readAll().toList().size());
         this.playerTeamPersistenceMongodb.delete(playerTeamList.get(0).getId());
         assertEquals(playerTeamListSize - 1, this.playerTeamPersistenceMongodb.readAll().toList().size());
     }
