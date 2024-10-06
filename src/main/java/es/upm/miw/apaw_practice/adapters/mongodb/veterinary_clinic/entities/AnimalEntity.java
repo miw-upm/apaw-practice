ebc8eae97a47d667d0f44c6ebd7207bb47f30e4f
rafaelacirino <rafaelaborbaf@gmail.com>
2024-10-06 19:01:52 +0200

    Creating DAOs and Entities #851

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/veterinary_clinic/entities/AnimalEntity.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/veterinary_clinic/entities/AnimalEntity.java
@@ -1,4 +1,85 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.veterinary_clinic.entities;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document
 public class AnimalEntity {
+
+    @DBRef
+    private OwnerEntity ownerEntity;
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String name;
+    private int age;
+    private LocalDateTime dateOfService;
+
+    public AnimalEntity() {
+        //empty from framework
+    }
+
+    public AnimalEntity(OwnerEntity ownerEntity, String name, int age, LocalDateTime dateOfService) {
+        this.ownerEntity = ownerEntity;
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.age = age;
+        this.dateOfService = dateOfService;
+    }
+
+    public OwnerEntity getOwnerEntity() {
+        return ownerEntity;
+    }
+
+    public void setOwnerEntity(OwnerEntity ownerEntity) {
+        this.ownerEntity = ownerEntity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDateTime getDateOfService() {
+        return dateOfService;
+    }
+
+    public void setDateOfService(LocalDateTime dateOfService) {
+        this.dateOfService = dateOfService;
+    }
+
+    @Override
+    public String toString() {
+        return "AnimalEntity{" +
+                "ownerEntity=" + ownerEntity +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", dateOfService=" + dateOfService +
+                '}';
+    }
 }
--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/veterinary_clinic/entities/OwnerEntity.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/veterinary_clinic/entities/OwnerEntity.java
@@ -1,4 +1,82 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.veterinary_clinic.entities;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document
 public class OwnerEntity {
+
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String name;
+    private String address;
+    private String phone;
+
+    public OwnerEntity() {
+        //empty from framework
+    }
+
+    public OwnerEntity(String name, String address, String phone) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return this == object || object != null && getClass() == object.getClass() &&
+                (name.equals(((OwnerEntity) object).name));
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "OwnerEntity{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
