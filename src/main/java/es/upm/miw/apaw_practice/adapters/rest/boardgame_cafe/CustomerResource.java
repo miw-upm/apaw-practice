898c400e4f7919992a7af32c1ae82820089b505f
joralvmel <joralvmel@gmail.com>
2024-10-07 14:57:43 +0200

    add endpoint POST Boardgame Cafe #876

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/boardgame_cafe/CustomerResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/boardgame_cafe/CustomerResource.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.adapters.rest.boardgame_cafe;
 
+import es.upm.miw.apaw_practice.domain.models.boardgame_cafe.Customer;
 import es.upm.miw.apaw_practice.domain.services.boardgame_cafe.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,5 +15,10 @@ public class CustomerResource {
     @Autowired
     public CustomerResource(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @PostMapping
+    public Customer create(@RequestBody Customer customer) {
+        return this.customerService.create(customer);
     }
 }
--- src/main/java/es/upm/miw/apaw_practice/domain/services/boardgame_cafe/CustomerService.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/services/boardgame_cafe/CustomerService.java
@@ -1,5 +1,6 @@
 package es.upm.miw.apaw_practice.domain.services.boardgame_cafe;
 
+import es.upm.miw.apaw_practice.domain.models.boardgame_cafe.Customer;
 import es.upm.miw.apaw_practice.domain.persistence_ports.boardgame_cafe.CustomerPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,11 @@ public class CustomerService {
     @Autowired
     public CustomerService(CustomerPersistence customerPersistence) {
         this.customerPersistence = customerPersistence;
+    }
+
+    @Autowired
+    public Customer create(Customer customer) {
+        return this.customerPersistence.create(customer);
     }
 
 }
