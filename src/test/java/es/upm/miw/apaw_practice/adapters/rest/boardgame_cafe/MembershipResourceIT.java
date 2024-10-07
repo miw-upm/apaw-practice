efe448dd6559f5f485e75d2f1b793faaf45bd5c5
joralvmel <joralvmel@gmail.com>
2024-10-07 14:26:29 +0200

    add endpoint GET tests Boardgame Cafe issue #875

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/boardgame_cafe/MembershipResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/boardgame_cafe/MembershipResourceIT.java
@@ -1,4 +1,45 @@
 package es.upm.miw.apaw_practice.adapters.rest.boardgame_cafe;
 
+import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.boardgame_cafe.Membership;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RestTestConfig
 public class MembershipResourceIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testReadMembership() {
+        this.webTestClient
+                .get()
+                .uri(MembershipResource.MEMBERSHIP + MembershipResource.MEMBERSHIPID_ID, 0 )
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Membership.class)
+                .value(Assertions::assertNotNull)
+                .value(membership -> {
+                    assertEquals(0, membership.getMembershipId());
+                    assertEquals("Gold", membership.getType());
+                    assertEquals(1, membership.getDuration());
+                    assertEquals(new BigDecimal("2.0"), membership.getDiscount());
+                });
+    }
+
+    @Test
+    void testReadNotFound() {
+        this.webTestClient
+                .get()
+                .uri(MembershipResource.MEMBERSHIP + MembershipResource.MEMBERSHIPID_ID, 999)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
 }
--- src/test/java/es/upm/miw/apaw_practice/domain/services/boardgame_cafe/MembershipServiceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/boardgame_cafe/MembershipServiceIT.java
@@ -1,4 +1,27 @@
 package es.upm.miw.apaw_practice.domain.services.boardgame_cafe;
 
+import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.boardgame_cafe.daos.MembershipRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.boardgame_cafe.entities.MembershipEntity;
+import es.upm.miw.apaw_practice.domain.models.boardgame_cafe.Membership;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestConfig
 public class MembershipServiceIT {
+
+    @Autowired
+    private MembershipService membershipService;
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @Test
+    void testReadById() {
+        MembershipEntity membership = this.membershipRepository.findAll().get(0);
+        Membership newMembershipEntity = this.membershipService.readByMembershipId(membership.getMembershipId());
+        assertEquals(membership.toMembership().getMembershipId(), newMembershipEntity.getMembershipId());
+    }
 }
