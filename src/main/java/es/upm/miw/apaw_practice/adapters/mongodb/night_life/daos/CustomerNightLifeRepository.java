4fab9b4a1a28a3e8cc875bbd2f4d1116b8cdb3e8
chanocb <101331282+chanocb@users.noreply.github.com>
2024-10-07 18:06:17 +0200

    Fixed DAOs #881

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/night_life/daos/CustomerNightLifeRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/night_life/daos/CustomerNightLifeRepository.java
@@ -3,5 +3,5 @@ package es.upm.miw.apaw_practice.adapters.mongodb.night_life.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.night_life.entities.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+public interface CustomerNightLifeRepository extends MongoRepository<CustomerEntity, String> {
 }
