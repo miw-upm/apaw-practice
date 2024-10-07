d9d09886ec2c77a68aeffe9a0b45804153691c9c
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-07 16:44:59 +0200

    'This is adding the Persistence of all the model

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/AppointmentPersistence.java
@@ -0,0 +1,7 @@
+package es.upm.miw.apaw_practice.domain.persistence_ports.Hospita;
+import org.springframework.stereotype.Repository;
+import java.util.stream.Stream;
+
+@Repository
+public  interface AppointmentPersistence {
+}
