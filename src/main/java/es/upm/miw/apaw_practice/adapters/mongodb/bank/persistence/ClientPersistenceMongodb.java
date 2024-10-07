23f2ea2067014b94d452c82cf48e0622d16c5e44
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2024-10-07 10:33:53 +0200

    Added persistece layers #845

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/bank/persistence/ClientPersistenceMongodb.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.bank.persistence;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.bank.ClientPersistence;
+import org.springframework.stereotype.Repository;
+
+@Repository("clientPersistence")
+public class ClientPersistenceMongodb implements ClientPersistence {
+}
