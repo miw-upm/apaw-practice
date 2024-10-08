76be51d15e7f4df8d4fb51c2aa03abb1c1e654a3
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-09 01:05:39 +0200

    In this commit is fixing PresitenceMongodb and Persitence#852

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/DoctorPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/DoctorPersistenceMongodb.java
@@ -3,7 +3,7 @@ package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.persistence;
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos.DoctorRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.DoctorEntities;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
-import es.upm.miw.apaw_practice.domain.models.Hospital.Article;
+
 import es.upm.miw.apaw_practice.domain.persistence_ports.Hospital.DoctorPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
