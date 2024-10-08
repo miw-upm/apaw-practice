a5a2b13324095b37622e6f2575d5a4037171ec89
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-08 03:41:37 +0200

    ïn his commit is fixing  the file entidy for my bd #852

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/entities/PatientEntity.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/entities/PatientEntity.java
@@ -1,94 +1,71 @@
-package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities;
-
+package es.upm.miw.apaw_practice.domain.models.Hospital;
 import es.upm.miw.apaw_practice.domain.models.Hospital.Appointment;
-import es.upm.miw.apaw_practice.domain.models.Hospital.Doctor;
-import es.upm.miw.apaw_practice.domain.models.Hospital.Patient;
-import org.springframework.beans.BeanUtils;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDate;
 
-import java.util.List;
+public class Patient {
+    private String dni;
+    private String fullname;
+    private LocalDate dateOfBirth;
+    private boolean hasInsurance;
+    private Appoinment appoinment;
 
-@Document(collection = "hospitals") // Indica que esta clase es una entidad de MongoDB
-public class HospitalEntity {
-    @Id
-    private String id; // ID del hospital
-    private String name;
-    private String location;
-    private int capacity;
-    private List<DoctorEntity> doctors;
-    private List<PatientEntity> patients;
-
-    public HospitalEntity() {
-
+    public Patient(String dni, String fullname, LocalDate dateOfBirth, boolean hasInsurance, Appoinment appoinment) {
+        this.dni = dni;
+        this.fullname = fullname;
+        this.dateOfBirth = dateOfBirth;
+        this.hasInsurance = hasInsurance;
+        this.appoinment = appoinment;
     }
 
-    public HospitalEntity(String id, String name, String location, int capacity, List<DoctorEntity> doctors, List<PatientEntity> patients) {
-        this.id = id;
-        this.name = name;
-        this.location = location;
-        this.capacity = capacity;
-        this.doctors = doctors;
-        this.patients = patients;
+    public String getDni() {
+        return dni;
     }
 
-    public String getId() {
-        return id;
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getFullname() {
+        return fullname;
     }
 
-    public String getName() {
-        return name;
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
     }
 
-    public String getLocation() {
-        return location;
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public boolean isHasInsurance() {
+        return hasInsurance;
     }
 
-    public int getCapacity() {
-        return capacity;
+    public void setHasInsurance(boolean hasInsurance) {
+        this.hasInsurance = hasInsurance;
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
+    public Appoinment getAppoinment() {
+        return appoinment;
     }
 
-    public List<DoctorEntity> getDoctors() {
-        return doctors;
-    }
-
-    public void setDoctors(List<DoctorEntity> doctors) {
-        this.doctors = doctors;
-    }
-
-    public List<PatientEntity> getPatients() {
-        return patients;
-    }
-
-    public void setPatients(List<PatientEntity> patients) {
-        this.patients = patients;
+    public void setAppoinment(Appoinment appoinment) {
+        this.appoinment = appoinment;
     }
 
     @Override
     public String toString() {
-        return "HospitalEntity{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", location='" + location + '\'' +
-                ", capacity=" + capacity +
-                ", doctors=" + doctors +
-                ", patients=" + patients +
+        return "Patient{" +
+                "dni='" + dni + '\'' +
+                ", fullname='" + fullname + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", hasInsurance=" + hasInsurance +
+                ", appoinment=" + appoinment +
                 '}';
     }
 }
+
