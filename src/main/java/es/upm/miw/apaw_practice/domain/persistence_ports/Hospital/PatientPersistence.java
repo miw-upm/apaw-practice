b76a50926c1824dd810bd07624fd60f1d913b97e
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-09 01:18:23 +0200

    In this commit is fixing PresitenceMongodb and Persitence of Hospital and Patient #852

---
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/PatientPersistence.java
@@ -0,0 +1,10 @@
+package es.upm.miw.apaw_practice.domain.persistence_ports.Hospital;
+
+import es.upm.miw.apaw_practice.domain.models.Hospital.Patient;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+@Repository
+public interface PatientPersistence {}
