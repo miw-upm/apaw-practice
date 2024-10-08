34717c70f02c57964381c4519e2ef69ebf81b83d
joralvmel <joralvmel@gmail.com>
2024-10-08 15:33:17 +0200

    add endpoint DELETE tests Boardgame Cafe #878

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/boardgame_cafe/GameResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/boardgame_cafe/GameResource.java
@@ -1,8 +1,11 @@
 package es.upm.miw.apaw_practice.adapters.rest.boardgame_cafe;
 
+import es.upm.miw.apaw_practice.domain.models.boardgame_cafe.Game;
 import es.upm.miw.apaw_practice.domain.services.boardgame_cafe.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping(GameResource.GAME)
@@ -15,6 +18,11 @@ public class GameResource {
     @Autowired
     public GameResource(GameService gameService) {
         this.gameService = gameService;
+    }
+
+    @GetMapping
+    public Stream<Game> readAllGames() {
+        return this.gameService.readAllGames();
     }
 
     @DeleteMapping(GAMENAME_ID)
