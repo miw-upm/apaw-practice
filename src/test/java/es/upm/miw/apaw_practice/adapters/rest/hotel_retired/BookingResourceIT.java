b047cfe252f353f3c421df076f79f2a31192a839
Miguel Ángel Del Fresno Rodríguez <madelfresno@gmail.com>
2024-10-08 17:45:38 +0200

    create POST endpoints #871

---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/hotel_retired/BookingResourceIT.java
@@ -0,0 +1,39 @@
+package es.upm.miw.apaw_practice.adapters.rest.hotel_retired;
+
+import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.hotel_retired.Booking;
+import es.upm.miw.apaw_practice.domain.models.hotel_retired.Guest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import java.awt.print.Book;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestTestConfig
+public class BookingResourceIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void testCreate() {
+        Guest guest = new Guest(
+                "99527370E",
+                "Emilio Pedrajas",
+                LocalDateTime.of(1990, 10, 27,  23, 2, 2 )
+        );
+        Booking booking = new Booking(false, LocalDate.now(), LocalDate.now().plusWeeks(1), guest);
+        this.webTestClient
+                .post()
+                .uri(BookingResource.BOOKINGS)
+                .body(BodyInserters.fromValue(booking))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Booking.class)
+                .value(Assertions::assertNotNull);
+    }
+}
