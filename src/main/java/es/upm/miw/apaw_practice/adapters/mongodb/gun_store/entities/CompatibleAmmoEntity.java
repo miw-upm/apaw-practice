9ad7145b28c844136bd57aedd1cef7fac9fe8e93
Saul.Osorio <Saul.Osorio@Solera.com>
2024-10-08 14:32:41 +0200

    Create entities model #849

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/gun_store/entities/CompatibleAmmoEntity.java
@@ -0,0 +1,55 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.gun_store.entities;
+
+import es.upm.miw.apaw_practice.domain.models.gun_store.CompatibleAmmo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Document
+public class CompatibleAmmoEntity {
+    @Id
+    Integer ammoId;
+    BigDecimal price;
+    private String caliber;
+
+    public CompatibleAmmoEntity() {
+        //Empty for framework
+    }
+
+    public CompatibleAmmoEntity(CompatibleAmmo compatibleAmmo) {
+        BeanUtils.copyProperties(compatibleAmmo, this);
+        this.ammoId = UUID.randomUUID().hashCode();
+    }
+
+    public Integer getAmmoId() {
+        return ammoId;
+    }
+
+    public void setAmmoId(Integer ammoId) {
+        this.ammoId = ammoId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCaliber() {
+        return caliber;
+    }
+
+    public void setCaliber(String caliber) {
+        this.caliber = caliber;
+    }
+
+    @Override
+    public String toString() {
+        return "CompatibleAmmoEntity{" + "price=" + price + ", ammoId=" + ammoId + ", caliber='" + caliber + '\'' + '}';
+    }
+}
