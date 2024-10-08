3f8b5c6a8d20bc2ad128c8f36e94485eece99a94
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-08 12:11:12 +0200

    ïn his commit is fixing the repository in DAOS #852

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos;
-import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.PatientEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PatientRepository extends MongoRepository<PatientEntity, String> {
-}
