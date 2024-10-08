639b0df1f70d9280f7d857ff29bbdade7d12a236
sergiobq0123 <s.sanchezp@alumnos.upm.es>
2024-10-08 20:15:03 +0200

    Add post Organization endpoint #914

--- src/test/java/es/upm/miw/apaw_practice/domain/services/competition/OrganizationServiceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/services/competition/OrganizationServiceIT.java
@@ -3,9 +3,11 @@ package es.upm.miw.apaw_practice.domain.services.competition;
 import es.upm.miw.apaw_practice.TestConfig;
 import es.upm.miw.apaw_practice.adapters.mongodb.competition.daos.OrganizationRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.competition.entities.OrganizationEntity;
+import es.upm.miw.apaw_practice.domain.models.competition.Organization;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,5 +32,13 @@ class OrganizationServiceIT {
         assertTrue(newOrganizationEntity.isPresent());
 
         assertEquals(oldIsInternational, !newOrganizationEntity.get().isInternational());
+    }
+
+    @Test
+    void testCreateOrganization() {
+         this.organizationService.createOrganization(new Organization("F.S. Barcelona", LocalDateTime.now(), false));
+
+        Optional<OrganizationEntity> newOrganizationEntity = this.organizationRepository.findByNameOrganization("F.S. Barcelona");
+        assertTrue(newOrganizationEntity.isPresent());
     }
 }
