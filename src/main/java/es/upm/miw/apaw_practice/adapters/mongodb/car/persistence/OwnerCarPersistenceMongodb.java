0550dc722a6516fb10c1a5e27e445e45207995e6
JAVIER ARANDA LOPEZ <javier.arandal@alumnos.upm.es>
2024-10-08 10:36:50 +0200

    update car repository test #879

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car/persistence/OwnerCarPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/car/persistence/OwnerCarPersistenceMongodb.java
@@ -3,6 +3,6 @@ package es.upm.miw.apaw_practice.adapters.mongodb.car.persistence;
 import es.upm.miw.apaw_practice.domain.persistence_ports.car.OwnerCarPersistence;
 import org.springframework.stereotype.Repository;
 
-@Repository("ownerPersistence")
+@Repository("ownerCarPersistence")
 public class OwnerCarPersistenceMongodb implements OwnerCarPersistence {
 }
