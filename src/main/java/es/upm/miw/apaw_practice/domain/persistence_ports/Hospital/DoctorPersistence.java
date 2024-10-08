92caa9141cf2f04fd9661ad6ba23709664be886f
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-09 01:02:49 +0200

    In this commit is fixing PresitenceMongodb and Persitence#852

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/DoctorPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/DoctorPersistence.java
@@ -1,6 +1,6 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.Hospital;
 
-import es.upm.miw.apaw_practice.domain.models.Hospital.Article;
+import es.upm.miw.apaw_practice.domain.models.Hospital.Doctor;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
