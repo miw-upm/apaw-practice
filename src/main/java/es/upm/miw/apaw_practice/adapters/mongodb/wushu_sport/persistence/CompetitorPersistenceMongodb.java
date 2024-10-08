7be977f6056aa7aede57a062c4d5d7daca276bbd
Adrian Alonso <Adrianalonso2001@gmail.com>
2024-10-08 14:18:32 +0200

    Test GetCompetitor #908

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/wushu_sport/persistence/CompetitorPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/wushu_sport/persistence/CompetitorPersistenceMongodb.java
@@ -1,11 +1,15 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.wushu_sport.persistence;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.competition.entities.CompetitionEntity;
 import es.upm.miw.apaw_practice.adapters.mongodb.wushu_sport.daos.CompetitorRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.wushu_sport.entities.CompetitorEntity;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.wuhshu_sport.Competitor;
 import es.upm.miw.apaw_practice.domain.persistence_ports.wushu_sport.CompetitorPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.stream.Stream;
 
 @Repository("CompetitorPersistence")
 public class CompetitorPersistenceMongodb implements CompetitorPersistence {
@@ -15,6 +19,12 @@ public class CompetitorPersistenceMongodb implements CompetitorPersistence {
     @Autowired
     public CompetitorPersistenceMongodb(CompetitorRepository competitorRepository) {
         this.competitorRepository = competitorRepository;
+    }
+
+    @Override
+    public Stream<Competitor> readAll() {
+        return this.competitorRepository.findAll().stream()
+                .map(CompetitorEntity::toCompetitor);
     }
 
     @Override
--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/wushu_sport/CompetitorPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/wushu_sport/CompetitorPersistence.java
@@ -3,8 +3,12 @@ package es.upm.miw.apaw_practice.domain.persistence_ports.wushu_sport;
 import es.upm.miw.apaw_practice.domain.models.wuhshu_sport.Competitor;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface CompetitorPersistence {
+
+    Stream<Competitor> readAll();
 
     Competitor readByLicence(String licence);
 
