9fec42956f03274eaa1e9ca432c919a9ee69c1ae
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-08 23:10:24 +0200

    In this commit is fixing the Persitence of mongodb #852

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-
-package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos;
-import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.DoctorEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DoctorRepository extends MongoRepository<DoctorEntity, String> {
-}
