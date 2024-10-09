e569d45095ebeaa2ae00e0bf6fa24a6638ce5814
chanocb <101331282+chanocb@users.noreply.github.com>
2024-10-09 11:48:16 +0200

    Added PUT Endpoint #918

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/night_life/CustomerNightLifePersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/night_life/CustomerNightLifePersistence.java
@@ -1,6 +1,10 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.night_life;
+import es.upm.miw.apaw_practice.domain.models.competition.TeamCompetition;
+import es.upm.miw.apaw_practice.domain.models.night_life.Customer;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerNightLifePersistence {
+    Customer readByName(String name);
     void delete(String name);
+    Customer update(String name, Customer customer);
 }
