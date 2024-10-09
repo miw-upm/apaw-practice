959724e61fab8caa7047f8b34f6d299896ae9fc0
sergiobq0123 <s.sanchezp@alumnos.upm.es>
2024-10-09 17:23:31 +0200

    Add GetCompetitionNameByPlayerId #925

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/competition/CompetitionResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/competition/CompetitionResource.java
@@ -8,11 +8,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(CompetitionResource.COMPETITION)
 public class CompetitionResource {
     static final String COMPETITION = "/competition";
     static final String ID_ID = "/{id}";
+    static final String TEAM_COMPETITION = "/team-competition";
+    static final String PLAYER_TEAM = "/player-team";
 
     private final CompetitionService competitionService;
 
@@ -25,5 +29,10 @@ public class CompetitionResource {
     @GetMapping(ID_ID)
     public Competition getCompetition(@PathVariable String id) {
         return this.competitionService.readById(id);
+    }
+
+    @GetMapping(TEAM_COMPETITION + PLAYER_TEAM + ID_ID)
+    public List<String> getNameCompetitionByPlayerId(@PathVariable String id) {
+        return this.competitionService.competitionNameByPlayerId(id);
     }
 }
