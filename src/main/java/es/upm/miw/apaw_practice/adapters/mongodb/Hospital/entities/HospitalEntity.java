e394fbe592d787a81c18deba2bdcaccbbe83bb18
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-11 13:16:44 +0200

    In this commit is fixing error #852

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/entities/HospitalEntity.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/entities/HospitalEntity.java
@@ -1,6 +1,8 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities;
 
 import es.upm.miw.apaw_practice.domain.models.Hospital.Hospital;
+import es.upm.miw.apaw_practice.domain.models.Hospital.Doctor;
+import es.upm.miw.apaw_practice.domain.models.Hospital.Patient;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
