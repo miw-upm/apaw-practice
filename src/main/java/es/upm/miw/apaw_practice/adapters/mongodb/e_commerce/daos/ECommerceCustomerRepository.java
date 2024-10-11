1f2966d000cf2daeded8c319e69cbc82263ade2d
Julián Sánchez <julian.sanchezfe@alumnos.upm.es>
2024-10-11 19:41:16 +0200

    Renames repositories of story e-commerce, Customer and ShoppingCart were colliding with the repositories of other stories #957

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/e_commerce/daos/ECommerceCustomerRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/e_commerce/daos/ECommerceCustomerRepository.java
@@ -2,8 +2,9 @@ package es.upm.miw.apaw_practice.adapters.mongodb.e_commerce.daos;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.e_commerce.entities.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.Optional;
 
-public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+public interface ECommerceCustomerRepository extends MongoRepository<CustomerEntity, String> {
     Optional<CustomerEntity> findByUserName(String userName);
 }
