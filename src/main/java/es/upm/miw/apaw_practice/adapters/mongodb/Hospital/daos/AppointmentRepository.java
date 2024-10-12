c33e0710f3e48816bea55a80360768db81db4cbe
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-12 11:36:28 +0200

    In this commit is fixing the class of seed #852

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/AppointmentRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/AppointmentRepository.java
@@ -3,7 +3,6 @@ package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.AppointmentEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
 public interface AppointmentRepository extends MongoRepository<AppointmentEntity, String> {
-
+    // Aquí puedes añadir métodos personalizados si es necesario
 }
