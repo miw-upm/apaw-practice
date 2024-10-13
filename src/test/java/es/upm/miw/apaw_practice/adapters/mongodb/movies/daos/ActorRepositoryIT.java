04a2c820413a6d3e266ab993d011bedfe657c825
victormartindz <victor.martin.dz@gmail.com>
2024-10-13 07:44:22 +0200

    created tests for Actor #911

---
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/movies/daos/ActorRepositoryIT.java
@@ -0,0 +1,32 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.movies.daos;
+
+import es.upm.miw.apaw_practice.TestConfig;
+import es.upm.miw.apaw_practice.adapters.mongodb.movies.entities.ActorEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestConfig
+class ActorRepositoryIT {
+
+    @Autowired
+    private ActorRepository actorRepository;
+
+    @Test
+    void testFindByArtisticNameNotExists() {
+        assertTrue(actorRepository.findByArtisticName("Alfredo Landa").isEmpty());
+    }
+
+    @Test
+    void testFindByArtisticName() {
+        assertTrue(this.actorRepository.findByArtisticName("Jack Nicholson").isPresent());
+        ActorEntity actor = this.actorRepository.findByArtisticName("Jack Nicholson").get();
+        assertEquals("Jack Nicholson", actor.getArtisticName());
+        assertEquals("John Joseph Nicholson", actor.getRealName());
+        assertFalse(actor.getAvailable());
+        assertEquals(LocalDate.of(1937, 4, 22), actor.getBirthDate());
+    }
+}
