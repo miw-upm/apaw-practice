52b1fb93267a9ab1181f1cad47a8fe9d7e1f9f48
Julián Sánchez <julian.sanchezfe@alumnos.upm.es>
2024-10-13 18:13:29 +0200

    Reverts class renaming in places that it was not needed (because belong to stories different to e-commerce) #957

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/boardgame_cafe/daos/CustomerRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/boardgame_cafe/daos/CustomerRepositoryIT.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
-class ECommerceCustomerRepositoryIT {
+class CustomerRepositoryIT {
 
     @Autowired
     private CustomerRepository customerRepository;
