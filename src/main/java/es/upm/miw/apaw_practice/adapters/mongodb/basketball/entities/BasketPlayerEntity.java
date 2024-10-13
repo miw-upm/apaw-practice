ae510364786d114f1f377687c720fd06ed8f3463
Martxel <56430705+MartxelAran@users.noreply.github.com>
2024-10-13 13:19:18 +0200

    optimize imports #970

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/basketball/entities/BasketPlayerEntity.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/basketball/entities/BasketPlayerEntity.java
@@ -6,8 +6,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Document
 public class BasketPlayerEntity {
     @Id
