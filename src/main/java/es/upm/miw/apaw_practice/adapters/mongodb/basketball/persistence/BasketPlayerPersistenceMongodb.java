58c011880ed27f5ee729f64c8ccbdfaa97077735
Martxel <56430705+MartxelAran@users.noreply.github.com>
2024-10-13 12:20:03 +0200

    add persistence layers #912

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/basketball/persistence/BasketPlayerPersistenceMongodb.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.basketball.persistence;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.basketball.BasketPlayerPersistence;
+import org.springframework.stereotype.Repository;
+
+@Repository("basketPlayerPersistence")
+public class BasketPlayerPersistenceMongodb implements BasketPlayerPersistence {
+}
---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/basketball/persistence/BasketSeasonPersistenceMongodb.java
@@ -0,0 +1,8 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.basketball.persistence;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.basketball.BasketSeasonPersistence;
+import org.springframework.stereotype.Repository;
+
+@Repository("basketSeasonPersistence")
+public class BasketSeasonPersistenceMongodb implements BasketSeasonPersistence {
+}
