52ec9eabce5740f10d9b8417002880ca52aba823
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-13 16:50:51 +0200

    In this commit is adding the endpoint GET #972

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/AppoinmentPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/AppoinmentPersistence.java
@@ -1,12 +1,12 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.hospital;
 
-import es.upm.miw.apaw_practice.domain.models.hospital.Appointment; // Asegúrate de que este paquete y clase existan
+import es.upm.miw.apaw_practice.domain.models.Hospital.Appointment; // Asegúrate de que este paquete y clase existan
 import org.springframework.stereotype.Repository;
 
 import java.util.stream.Stream;
 
 @Repository
-public interface AppointmentPersistence {
+public interface AppoinmentPersistence {
 
     Appointment read(String id); // Método para leer una cita específica por ID, si es necesario
 }
