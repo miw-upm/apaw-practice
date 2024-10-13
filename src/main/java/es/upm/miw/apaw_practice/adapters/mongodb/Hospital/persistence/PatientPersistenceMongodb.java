7da981be2a018957ee4889864e78b3ed2d7f1a61
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-13 12:47:10 +0200

    In this commit is fixing the class of seed #852

---
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/PatientPersistenceMongodb.java
@@ -0,0 +1,17 @@
+package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.persistence;
+
+
+import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
+
+import es.upm.miw.apaw_practice.domain.persistence_ports.Hospital.ShoppingCartPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository("shoppingCartPersistence")
+public class PatientPersistenceMongodb implements ShoppingCartPersistence {
+
+}
