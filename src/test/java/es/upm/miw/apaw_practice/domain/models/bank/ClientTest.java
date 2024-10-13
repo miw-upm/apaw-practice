c8d0bca0e08a81a200bd4417bf19757338280024
alvaro.avilesr <alvaro.avilesr@alumnos.upm.es>
2024-10-14 01:50:14 +0200

    Fixed code smells #936

--- src/test/java/es/upm/miw/apaw_practice/domain/models/bank/ClientTest.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/models/bank/ClientTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
-public class ClientTest {
+class ClientTest {
 
     @Test
     void testBuilder() {
--- src/test/java/es/upm/miw/apaw_practice/domain/models/bank/TreeClientTest.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/models/bank/TreeClientTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestConfig
-public class TreeClientTest {
+class TreeClientTest {
 
     private TreeClientLeaf treeClientLeaf1;
     private TreeClientLeaf treeClientLeaf2;
