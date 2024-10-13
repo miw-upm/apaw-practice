8f73d2dee374c098bdf597077b5d1966adf4b8d1
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-13 03:11:13 +0200

    In this commit is fixing the class of seed #852

--- src/main/java/es/upm/miw/apaw_practice/domain/models/Hospital/Doctor.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/Hospital/Doctor.java
@@ -6,14 +6,16 @@ public class Doctor {
     private String dni;
     private String fullname;
     private BigDecimal salary;
+    private String specialty;
 
     public Doctor() {
     }
 
-    public Doctor(String dni, String fullname, BigDecimal salary) {
+    public Doctor(String dni, String fullname, BigDecimal salary,specialty) {
         this.dni = dni;
         this.fullname = fullname;
         this.salary = salary;
+        this.specialty=specialty;
     }
 
     // Getters
@@ -29,12 +31,33 @@ public class Doctor {
         return salary;
     }
 
-    @Override
-    public String toString() {
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
         return "Doctor{" +
                 "dni='" + dni + '\'' +
                 ", fullname='" + fullname + '\'' +
                 ", salary=" + salary +
+                ", specialty='" + specialty + '\'' +
                 '}';
     }
 }
