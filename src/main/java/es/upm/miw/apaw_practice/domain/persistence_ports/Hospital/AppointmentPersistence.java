87395853b77c482e2127a14c9fd15b1f12902084
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-15 01:09:37 +0200

    In this commit is adding the endpoint GET #972

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/AppointmentPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/Hospital/AppointmentPersistence.java
@@ -1,10 +1,10 @@
-package es.upm.miw.apaw_practice.domain.persistence_ports.Hospita;
+package es.upm.miw.apaw_practice.domain.persistence_ports.Hospital;
 
 import java.util.List;
 import java.util.Optional;
 import es.upm.miw.apaw_practice.domain.models.Hospital.Appointment;
-
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface AppoinmentPersistence {
     List<Appointment> findAll();
 
