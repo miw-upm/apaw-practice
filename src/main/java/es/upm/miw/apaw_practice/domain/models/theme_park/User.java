7534acd17d4eea49c388bc1e32e752087af73e76
orosario <o.rosario@alumnos.upm.es>
2024-10-15 00:35:35 +0200

    Added Pattern 1 Builder #987

--- src/main/java/es/upm/miw/apaw_practice/domain/models/theme_park/User.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/theme_park/User.java
@@ -1,5 +1,7 @@
 package es.upm.miw.apaw_practice.domain.models.theme_park;
 
+import es.upm.miw.apaw_practice.domain.models.theme_park.UserBuilder;
+
 import java.time.LocalDateTime;
 
 public class User {
@@ -7,6 +9,10 @@ public class User {
     private String address;
     private LocalDateTime entranceDate;
     private Boolean oneYearMembership;
+
+    public static UserBuilder.IdMembership builder() {
+        return new Builder();
+    }
 
     public User() {
         //empty for framework
@@ -59,5 +65,43 @@ public class User {
                 ", entranceDate='" + entranceDate + '\'' +
                 ", oneYearMembership='" + oneYearMembership + '\'' +
                 '}';
+    }
+
+    public static class Builder implements UserBuilder.IdMembership, UserBuilder.Address, UserBuilder.EntranceDate, UserBuilder.OneYearMembership, UserBuilder.Builder {
+
+        private final User instance;
+
+        private Builder() {
+            instance = new User();
+        }
+
+        @Override
+        public UserBuilder.Address idMembership(String idMembership) {
+            instance.idMembership = idMembership;
+            return this;
+        }
+
+        @Override
+        public UserBuilder.EntranceDate address(String address) {
+            instance.address = address;
+            return this;
+        }
+
+        @Override
+        public UserBuilder.OneYearMembership entranceDate(LocalDateTime entranceDate) {
+            instance.entranceDate = entranceDate;
+            return this;
+        }
+
+        @Override
+        public UserBuilder.Builder oneYearMembership(Boolean oneYearMembership) {
+            instance.oneYearMembership = oneYearMembership;
+            return this;
+        }
+
+        @Override
+        public User build() {
+            return instance;
+        }
     }
 }
