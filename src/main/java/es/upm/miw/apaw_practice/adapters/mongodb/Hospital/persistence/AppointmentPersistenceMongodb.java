b2bc907ec483e62bbc4d5874c6e6a13e7ffda711
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-15 01:14:56 +0200

    In this commit is adding the endpoint GET #972

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/AppointmentPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/AppointmentPersistenceMongodb.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository("appointmentPersistence")
-public class AppointmentPersistenceMongodb implements AppoinmentPersistence {
+public class AppointmentPersistenceMongodb implements AppointmentPersistence {
 
     private final AppointmentRepository appointmentRepository;
 
