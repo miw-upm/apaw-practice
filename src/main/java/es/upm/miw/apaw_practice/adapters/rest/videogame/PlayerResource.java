9a36a6447e13dfd4725b7b432e0a74ab8b770b78
LuisLoboR16 <Lwolf95@hotmail.com>
2024-10-15 12:19:42 -0500

    added DELETE test for endpoint #1031

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame/PlayerResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame/PlayerResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(PlayerResource.PLAYERS)
 public class PlayerResource {
     public static final String PLAYERS = "/players";
 
