fe02e16d35cdd5771224d68a287b53add37b95e0
orosario <o.rosario@alumnos.upm.es>
2024-10-16 00:11:30 +0200

    Added persistence for search 1 #985

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/theme_park/persistence/ThemeParkPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/theme_park/persistence/ThemeParkPersistenceMongodb.java
@@ -1,6 +1,9 @@
 package es.upm.miw.apaw_practice.adapters.mongodb.theme_park.persistence;
 
+import es.upm.miw.apaw_practice.adapters.mongodb.theme_park.daos.OperatorRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.theme_park.daos.ThemeParkRepository;
+import es.upm.miw.apaw_practice.adapters.mongodb.theme_park.entities.OperatorEntity;
+import es.upm.miw.apaw_practice.adapters.mongodb.theme_park.entities.RideEntity;
 import es.upm.miw.apaw_practice.adapters.mongodb.theme_park.entities.ThemeParkEntity;
 import es.upm.miw.apaw_practice.domain.exceptions.NotFoundException;
 import es.upm.miw.apaw_practice.domain.models.theme_park.ThemePark;
@@ -9,16 +12,19 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.stream.Stream;
 
 @Repository("themeParkPersistence")
 public class ThemeParkPersistenceMongodb implements ThemeParkPersistence {
 
     private final ThemeParkRepository themeParkRepository;
-
+    private final OperatorRepository operatorRepository;
     @Autowired
-    public ThemeParkPersistenceMongodb(ThemeParkRepository themeParkRepository) {
+    public ThemeParkPersistenceMongodb(ThemeParkRepository themeParkRepository, OperatorRepository operatorRepository) {
         this.themeParkRepository = themeParkRepository;
+        this.operatorRepository = operatorRepository;
     }
 
     @Override
@@ -44,5 +50,22 @@ public class ThemeParkPersistenceMongodb implements ThemeParkPersistence {
         BeanUtils.copyProperties(themePark, themeParkEntity, "id");
         return this.themeParkRepository.save(themeParkEntity)
                 .toThemePark();
+    }
+
+    @Override
+    public BigDecimal getSumPriceByNick(String nick){
+        List<RideEntity> rides = this.operatorRepository.findAll()
+                .stream()
+                .filter(operatorEntity -> operatorEntity.getNick().equals(nick))
+                .map(OperatorEntity::getRideEntity)
+                .toList();
+
+        return this.themeParkRepository.findAll()
+                .stream()
+                .filter(themePark -> themePark.getRideEntities().stream().anyMatch(rides::contains))
+                .map(ThemeParkEntity::toThemePark)
+                .distinct()
+                .map(ThemePark::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
