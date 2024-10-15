af58e3756db630b19bb9c5650517d40aaae89823
Martxel <56430705+MartxelAran@users.noreply.github.com>
2024-10-15 21:15:53 +0200

    added POST endpoint and BasketMatch DAO test #971

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/basketball/daos/BasketMatchRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/basketball/daos/BasketMatchRepository.java
@@ -3,5 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.basketball.daos;
 import es.upm.miw.apaw_practice.adapters.mongodb.basketball.entities.BasketMatchEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface BasketMatchRepository extends MongoRepository<BasketMatchEntity, String> {
+    List<BasketMatchEntity> findByMatchIdIn(List<Integer> idList);
 }
