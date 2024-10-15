5c679ebced986b1f83a90ab08221f3869196c41a
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-15 12:48:00 +0200

    In this commit is adding the endpoint POST IN HOSPITAL #1017

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/HospitalRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/HospitalRepository.java
@@ -1,26 +1,11 @@
-package es.upm.miw.apaw_practice.adapters.rest.Hospital;
+package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos;
 
-import es.upm.miw.apaw_practice.domain.models.Hospital.Hospital;
-import es.upm.miw.apaw_practice.domain.services.Hospital.HospitalService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.HospitalEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@RestController
-@RequestMapping(HospitalResource.HOSPITALS)
-public class HospitalResource {
+import java.time.LocalDate;
+import java.util.List;
 
-    static final String HOSPITALS = "/hospital";
+public interface HospitalRepository extends MongoRepository<HospitalEntity, Integer> {
 
-    private final HospitalService hospitalService;
-
-    @Autowired
-    public HospitalResource(HospitalService hospitalService) {
-        this.hospitalService = hospitalService;
-    }
-
-    // Endpoint to create a hospital
-    @PostMapping
-    public Hospital createHospital(@RequestBody Hospital hospital) {
-        return this.hospitalService.createHospital(hospital);
-    }
 }
