f913f1f162271e410148f1c7838474aecae865bd
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-15 13:04:06 +0200

    In this commit is adding the endpoint POST IN HOSPITAL #1017

--- src/main/java/es/upm/miw/apaw_practice/domain/models/Hospital/Hospital.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/Hospital/Hospital.java
@@ -1,69 +1,47 @@
 package es.upm.miw.apaw_practice.domain.models.Hospital;
+
 import java.util.List;
 
 public class Hospital {
+    private String id;
     private String name;
     private String location;
-    private int capacity;
-    private List<Doctor> Doctores;
+    private Integer capacity;
+    private List<Doctor> doctors;
     private List<Patient> patients;
 
-    public Hospital(String name, String location, int capacity, List<Doctor> doctores, List<Patient> patients) {
+    // Constructor
+    public Hospital(String id, String name, String location, Integer capacity, List<Doctor> doctors, List<Patient> patients) {
+        this.id = id;
         this.name = name;
         this.location = location;
         this.capacity = capacity;
-        this.Doctores = doctores;
-        this.patients = patients;
+        this.doctors = doctors;
+        this.patients = patients; 
+    }
+
+    // Métodos get
+    public String getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getLocation() {
         return location;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public int getCapacity() {
+    public Integer getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public List<Doctor> getDoctores() {
-        return Doctores;
-    }
-
-    public void setDoctores(List<Doctor> doctores) {
-        Doctores = doctores;
+    public List<Doctor> getDoctors() {
+        return doctors;
     }
 
     public List<Patient> getPatients() {
         return patients;
-    }
-
-    public void setPatients(List<Patient> patients) {
-        this.patients = patients;
-    }
-
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Hospital{" +
-                "name='" + name + '\'' +
-                ", location='" + location + '\'' +
-                ", capacity=" + capacity +
-                ", Doctores=" + Doctores +
-                ", patients=" + patients +
-                '}';
     }
 }
