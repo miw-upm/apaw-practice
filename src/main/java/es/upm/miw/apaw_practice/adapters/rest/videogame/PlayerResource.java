be30e4ce9c02655905de0404ec00c5becde040bb
LuisLoboR16 <Lwolf95@hotmail.com>
2024-10-15 11:21:17 -0500

    added DELETE endpoint #1031

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame/PlayerResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/videogame/PlayerResource.java
@@ -1,4 +1,28 @@
 package es.upm.miw.apaw_practice.adapters.rest.videogame;
 
+import es.upm.miw.apaw_practice.domain.services.videogame.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class PlayerResource {
+    public static final String PLAYERS = "/players";
+
+    static final String PLAYER_NAME = "/{playerName}";
+
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerResource(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @DeleteMapping(PLAYER_NAME)
+    public void delete(@PathVariable String playerName) {
+        this.playerService.delete(playerName);
+    }
 }
