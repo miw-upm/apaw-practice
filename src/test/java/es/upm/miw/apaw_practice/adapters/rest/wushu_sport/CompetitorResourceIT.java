70745cfa3cd55cd4942ddc7a0e8048e621170713
Adrian Alonso <Adrianalonso2001@gmail.com>
2024-10-16 21:24:32 +0200

    tests #1048

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/wushu_sport/CompetitorResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/wushu_sport/CompetitorResourceIT.java
@@ -1,9 +1,13 @@
 package es.upm.miw.apaw_practice.adapters.rest.wushu_sport;
 
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.wuhshu_sport.Competitor;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RestTestConfig
 public class CompetitorResourceIT {
@@ -14,11 +18,25 @@ public class CompetitorResourceIT {
     private CompetitorResource competitorResource;
 
     @Test
-    void testGetCompetitor(){
+    void testGetCompetitorNotFound(){
         this.webTestClient
                 .get()
                 .uri(CompetitorResource.COMPETITOR + CompetitorResource.LICENCE_ID, "NO/LICENCE")
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+    @Test
+    void testGetCompetitor(){
+        this.webTestClient
+                .get()
+                .uri(CompetitorResource.COMPETITOR + CompetitorResource.LICENCE_ID, "WU/A/00126")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Competitor.class)
+                .value(Assertions::assertNotNull)
+                .value(competitor -> {
+                    assertEquals(1, competitor.getFederatedYears());
+                });
+
     }
 }
