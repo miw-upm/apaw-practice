2a84cae852c961e11f0f053654240c3918da5c4f
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-16 16:53:52 +0200

    In this commit is adding the endpoint UPDATE IN HOSPITAL #1018

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/DoctorRepository.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/daos/DoctorRepository.java
@@ -2,8 +2,9 @@ package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.DoctorEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DoctorRepository extends MongoRepository<DoctorEntity, String> {
-    // Método para verificar si existe un doctor por su DNI
-    boolean existsByDni(String dni)
+    boolean existsByDni(String dni);
 }
