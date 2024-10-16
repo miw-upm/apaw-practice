dbd91cd6a841c201908b776de18992d464e9712f
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-16 16:46:20 +0200

    In this commit is adding the endpoint UPDATE IN HOSPITAL #1018

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/Hospital/DoctorResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/Hospital/DoctorResource.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/doctors")
-public class DoctorController {
+public class DoctorResource {
 
     private final DoctorPersistence doctorPersistence;
 
