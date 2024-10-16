b7afc8b301d3c6aa328419ca4d0d026a0db54990
Miguel Ángel Del Fresno Rodríguez <madelfresno@gmail.com>
2024-10-17 00:34:22 +0200

    added Pattern 2 for Room model #1010

--- src/main/java/es/upm/miw/apaw_practice/domain/models/hotel_retired/Accomodation.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/models/hotel_retired/Accomodation.java
@@ -1,4 +1,20 @@
 package es.upm.miw.apaw_practice.domain.models.hotel_retired;
 
+import java.math.BigDecimal;
+
 public interface Accomodation {
+
+    void add(Accomodation accomodation);
+
+    void remove(Accomodation accomodation);
+
+    String getNum();
+
+    int getNumBeds();
+
+    Boolean getOccupied();
+
+    BigDecimal getPrice();
+
+    boolean isComposite();
 }
--- src/test/java/es/upm/miw/apaw_practice/domain/models/hotel_retired/RoomTest.java
+++ src/test/java/es/upm/miw/apaw_practice/domain/models/hotel_retired/RoomTest.java
@@ -1,4 +1,54 @@
 package es.upm.miw.apaw_practice.domain.models.hotel_retired;
 
+import es.upm.miw.apaw_practice.TestConfig;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestConfig
 public class RoomTest {
+
+    @Test
+    void testRoomLeaf() {
+        Accomodation room = new Room(
+                "100",
+                false,
+                3,
+                BigDecimal.valueOf(59.99),
+                Collections.emptyList()
+        );
+
+        assertFalse(room.isComposite());
+    }
+
+    @Test
+    void testSuiteComposite() {
+        Accomodation room = new Room(
+                "100",
+                false,
+                2,
+                BigDecimal.valueOf(59.99),
+                Collections.emptyList()
+        );
+
+        Accomodation room2 = new Room(
+                "101",
+                true,
+                4,
+                BigDecimal.valueOf(99.99),
+                Collections.emptyList()
+        );
+        Accomodation suite = new Suite();
+        suite.add(room);
+        suite.add(room2);
+
+        assertTrue(suite.isComposite());
+        assertTrue(suite.getOccupied());
+        assertEquals(BigDecimal.valueOf(159.98), suite.getPrice());
+        assertEquals("100-101", suite.getNum());
+        assertEquals(6, suite.getNumBeds());
+    }
 }
