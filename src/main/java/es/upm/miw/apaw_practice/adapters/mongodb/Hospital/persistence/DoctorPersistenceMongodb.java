f95d8cf8d59066fb756aa33c18f08bff980de5e4
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-16 16:58:08 +0200

    In this commit is adding the endpoint UPDATE IN HOSPITAL #1018

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/DoctorPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/DoctorPersistenceMongodb.java
@@ -3,6 +3,8 @@ package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.persistence;
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos.DoctorRepository;
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.DoctorEntity;
 import es.upm.miw.apaw_practice.domain.models.Hospital.Doctor;
+import es.upm.miw.apaw_practice.exceptions.NotFoundException; // Importa la clase NotFoundException
+
 import es.upm.miw.apaw_practice.domain.persistence_ports.Hospital.DoctorPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
