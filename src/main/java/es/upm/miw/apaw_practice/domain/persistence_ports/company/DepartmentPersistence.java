4347b6b80f239595e70cefb5948af93207680087
zhuha <zhuhaoyyy@gmail.com>
2024-10-17 18:51:12 +0200

    Add PUT END-POINT COMPANY AND RELATED TESTS #1049

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/company/DepartmentPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/company/DepartmentPersistence.java
@@ -1,7 +1,10 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.company;
 
+import es.upm.miw.apaw_practice.domain.models.company.Department;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentPersistence {
+
+    Department updateEmployeeCount(String departmentname, Integer employeeCount);
 }
