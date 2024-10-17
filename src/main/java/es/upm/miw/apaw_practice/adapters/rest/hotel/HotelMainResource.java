1b8f89d7025ecf2ee28f9550aba36703383ae3e8
Hongxiang Wu <90578041+Aklur@users.noreply.github.com>
2024-10-17 16:35:21 +0200

    Added new tests for Get endpoint

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/hotel/HotelMainResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/hotel/HotelMainResource.java
@@ -4,26 +4,23 @@ import es.upm.miw.apaw_practice.adapters.rest.LexicalAnalyzer;
 import es.upm.miw.apaw_practice.domain.models.hotel.HotelMain;
 import es.upm.miw.apaw_practice.domain.services.hotel.HotelMainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(HotelMainResource.HOTELS)
 public class HotelMainResource {
 
     static final String HOTELS = "/hotel/hotels";
-
+    static final String NAMES= "/{name}";
     private final HotelMainService hotelMainService;
 
     @Autowired
     public HotelMainResource(HotelMainService hotelMainService) {
             this.hotelMainService = hotelMainService;
         }
-    @GetMapping
-    public HotelMain findByName(@RequestParam String q) {
-        String name = new LexicalAnalyzer().extractWithAssure(q,"name");
+
+    @GetMapping(NAMES)
+    public HotelMain findByName(@PathVariable String name) {
         return this.hotelMainService.findByName(name);
     }
 }
