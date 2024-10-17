d57dd9508fdf662672bb7568d8db78bf9cd0eb62
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-17 19:24:45 +0200

    In this commit is adding the endpoint PATCH IN HOSPITAL #1018

--- src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/PatientPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/PatientPersistenceMongodb.java
@@ -2,6 +2,7 @@ package es.upm.miw.apaw_practice.adapters.mongodb.Hospital.persistence;
 
 import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.daos.PatientRepository;
 import es.upm.miw.apaw_practice.domain.persistence_ports.Hospital.PatientPersistence;
+import es.upm.miw.apaw_practice.adapters.mongodb.Hospital.entities.PatientEntity;
 import es.upm.miw.apaw_practice.domain.models.Hospital.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
