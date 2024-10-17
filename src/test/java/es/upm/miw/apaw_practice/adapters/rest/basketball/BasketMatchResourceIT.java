b3190de04e0dcd19c19b168d7c6c5a2a7909450d
Martxel <56430705+MartxelAran@users.noreply.github.com>
2024-10-17 20:36:43 +0200

    added PATCH endpoint tests and remove unused imports #974

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/basketball/BasketMatchResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/basketball/BasketMatchResourceIT.java
@@ -1,9 +1,14 @@
 package es.upm.miw.apaw_practice.adapters.rest.basketball;
 
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
+import es.upm.miw.apaw_practice.domain.models.basketball.BasketMatch;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RestTestConfig
 public class BasketMatchResourceIT {
@@ -18,5 +23,28 @@ public class BasketMatchResourceIT {
                 .uri(BasketMatchResource.MATCHES + BasketMatchResource.ID_ID, 5)
                 .exchange()
                 .expectStatus().isOk();
+    }
+
+    @Test
+    void testUpdateNotFound() {
+        this.webTestClient
+                .patch()
+                .uri(BasketMatchResource.MATCHES + BasketMatchResource.ID_ID, 10)
+                .body(BodyInserters.fromValue("Parque de Vallecas"))
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testUpdate() {
+        this.webTestClient
+                .patch()
+                .uri(BasketMatchResource.MATCHES + BasketMatchResource.ID_ID, 7)
+                .body(BodyInserters.fromValue("Parque pacifico"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BasketMatch.class)
+                .value(Assertions::assertNotNull)
+                .value(basketMatch -> assertEquals("Parque pacifico",basketMatch.getAddress()));
     }
 }
