4fb9565a910cadb019c1e7a526e240472bd403e7
Martxel <56430705+MartxelAran@users.noreply.github.com>
2024-10-17 20:12:50 +0200

    added PATCH endpoint #974

--- src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/basketball/BasketMatchPersistence.java
+++ src/main/java/es/upm/miw/apaw_practice/domain/persistence_ports/basketball/BasketMatchPersistence.java
@@ -1,8 +1,11 @@
 package es.upm.miw.apaw_practice.domain.persistence_ports.basketball;
 
+import es.upm.miw.apaw_practice.domain.models.basketball.BasketMatch;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BasketMatchPersistence {
     void delete(Integer id);
+
+    BasketMatch updateAddress(Integer matchId, String address);
 }
