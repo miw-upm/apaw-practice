7f9e4fb4fe7af077251291c0644016c37b6a92ae
TrueAlexCv <alexcvtrue24282@gmail.com>
2024-10-18 10:40:06 +0200

    Added resource and test SEARCH 1 #1054

--- src/test/java/es/upm/miw/apaw_practice/adapters/rest/shopping_center/TicketResourceIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/rest/shopping_center/TicketResourceIT.java
@@ -2,6 +2,8 @@ package es.upm.miw.apaw_practice.adapters.rest.shopping_center;
 
 import es.upm.miw.apaw_practice.adapters.rest.RestTestConfig;
 import es.upm.miw.apaw_practice.domain.models.shopping_center.Ticket;
+import org.hamcrest.number.BigDecimalCloseTo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -43,5 +45,31 @@ public class TicketResourceIT {
                 .body(BodyInserters.fromValue(newTotalValue))
                 .exchange()
                 .expectStatus().isOk();
+    }
+
+    @Test
+    void testSumTotalPriceOfMainService() {
+        this.webTestClient
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path(TicketResource.TICKETS + TicketResource.SEARCH)
+                                .queryParam("q", "mainService:tools")
+                                .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(BigDecimal.class)
+                .value(Assertions::assertNotNull);
+    }
+
+    @Test
+    void testSumTotalPriceOfMainServiceBadRequest() {
+        this.webTestClient
+                .get()
+                .uri(uriBuilder ->
+                        uriBuilder.path(TicketResource.TICKETS + TicketResource.SEARCH)
+                                .queryParam("q", "secondService:food")
+                                .build())
+                .exchange()
+                .expectStatus().isBadRequest();
     }
 }
