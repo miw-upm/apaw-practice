ac16d8b0419cbee864e3be2ca4c4743db769dba3
BrayanVera <brayanpablo@outlook.com>
2024-10-18 02:47:52 +0200

    feat - end POST and Test of UserCourse create #969

--- src/main/java/es/upm/miw/apaw_practice/adapters/rest/course/UserCourseResource.java
+++ src/main/java/es/upm/miw/apaw_practice/adapters/rest/course/UserCourseResource.java
@@ -1,7 +1,10 @@
 package es.upm.miw.apaw_practice.adapters.rest.course;
 
-import es.upm.miw.apaw_practice.domain.services.theme_park.UserService;
+import es.upm.miw.apaw_practice.domain.models.course.User;
+import es.upm.miw.apaw_practice.domain.services.course.UserCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,11 +14,16 @@ public class UserCourseResource {
 
     static final String USERS = "/course/users";
 
-    private final UserService userService;
+    private final UserCourseService userCourseService;
 
     @Autowired
-
-    public UserCourseResource(UserService userService) {
-        this.userService = userService;
+    public UserCourseResource(UserCourseService userCourseService) {
+        this.userCourseService = userCourseService;
     }
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return this.userCourseService.create(user);
+    }
+
 }
