9af6ee12546649d2a9d8a10122b43213e73d45a1
estedosunidos <ivaneestebanvargasmartines@gmail.com>
2024-10-19 23:28:28 +0200

    In this commit is adding the test #1025

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/HospitalMongodbIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/HospitalMongodbIT.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
-public class HospitalPersistenceMongodbTest {
+public class HospitalMongodbIT {
 
     @Mock
     private HospitalRepository hospitalRepository;
--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/PatientMongodbIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/Hospital/persistence/PatientMongodbIT.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class PatientPersistenceMongodbTest {
+public class PatientMongodbIT {
 
     @Mock
     private PatientRepository patientRepository;
