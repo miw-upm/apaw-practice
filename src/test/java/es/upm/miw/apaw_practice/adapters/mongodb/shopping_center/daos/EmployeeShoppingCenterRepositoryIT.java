967c8d55f5a82a66327d4046513f2f287c01645e
TrueAlexCv <alexcvtrue24282@gmail.com>
2024-10-21 14:17:00 +0200

    Improve quality and coverage in adapters mongodb #1103

--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/shopping_center/daos/EmployeeShoppingCenterRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/shopping_center/daos/EmployeeShoppingCenterRepositoryIT.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestConfig
-public class EmployeeShoppingCenterRepositoryIT {
+class EmployeeShoppingCenterRepositoryIT {
 
     @Autowired
     private EmployeeShoppingCenterRepository employeeShoppingCenterRepository;
--- src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/shopping_center/daos/ProviderRepositoryIT.java
+++ src/test/java/es/upm/miw/apaw_practice/adapters/mongodb/shopping_center/daos/ProviderRepositoryIT.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestConfig
-public class ProviderRepositoryIT {
+class ProviderRepositoryIT {
 
     @Autowired
     private ProviderRepository providerRepository;
