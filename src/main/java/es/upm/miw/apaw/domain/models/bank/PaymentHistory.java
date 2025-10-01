9aeb269a77685729fd46ce1628da3044be85aab7
JialeWangXu <jiale.wang@alumnos.upm.es>
2025-10-02 01:07:42 +0200

    Added Bank Model #1158

---
+++ src/main/java/es/upm/miw/apaw/domain/models/bank/PaymentHistory.java
@@ -0,0 +1,29 @@
+package es.upm.miw.apaw.domain.models.bank;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentHistory {
+
+    @NotNull
+    private UUID historyId;
+    @NotNull
+    private BigDecimal paymentAmount;
+    @NotNull
+    private LocalDateTime paymentDate;
+    @NotNull
+    private Boolean paid;
+    @NotNull
+    private CreditCard transactionCard;
+}
