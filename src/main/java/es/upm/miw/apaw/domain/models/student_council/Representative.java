d8effb256eaeba21618539ac557b37c6168b8f6e
DaniYDevs <danidzm18@gmail.com>
2025-10-02 18:20:48 +0200

    Added Student Council Model and UML #1163

---
+++ src/main/java/es/upm/miw/apaw/domain/models/student_council/Representative.java
@@ -0,0 +1,30 @@
+package es.upm.miw.apaw.domain.models.student_council;
+
+import es.upm.miw.apaw.domain.models.UserDto;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Representative {
+
+    @NotNull
+    private UUID representativeId;
+
+    @NotNull
+    private LocalDateTime assignmentDate;
+
+    @NotNull
+    private String representativeRole;
+
+    @NotNull
+    private UserDto user;
+
+    private List<StudentIssue> issues;
+}
