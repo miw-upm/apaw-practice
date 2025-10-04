b215a3b001010e4b281e85dc192b8d5694217e3a
SihuiXu <s.xu@alumnos.upm.es>
2025-10-04 11:06:25 +0200

    fix classes #1159

--- src/main/java/es/upm/miw/apaw/domain/models/videogame/LikeList.java
+++ src/main/java/es/upm/miw/apaw/domain/models/videogame/LikeList.java
@@ -2,9 +2,7 @@ package es.upm.miw.apaw.domain.models.videogame;
 
 import es.upm.miw.apaw.domain.models.UserDto;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
