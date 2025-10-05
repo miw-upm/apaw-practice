fb887b80b34eb0073692d2bbbcd4582b6789a8ac
jacardenas <juanancardenas@gmail.com>
2025-10-05 19:00:44 +0200

    added testing position recruiting #1201

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/recruiting/daos/PositionRepository.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/recruiting/daos/PositionRepository.java
@@ -2,13 +2,11 @@ package es.upm.miw.apaw.adapters.mongodb.recruiting.daos;
 
 import es.upm.miw.apaw.adapters.mongodb.recruiting.entities.PositionEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface PositionRepository extends MongoRepository<PositionEntity, UUID> {
 
-    @Query(sort = "{ reference: -1 }")
     Optional<PositionEntity> findTopByOrderByReferenceDesc();
 }
