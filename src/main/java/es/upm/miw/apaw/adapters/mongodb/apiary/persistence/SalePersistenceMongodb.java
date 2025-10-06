3827dee25fa389cb60dad2a0c5b4b594ef4119c1
paulixx <paula.martinez.arce@alumnos.upm.es>
2025-10-06 12:19:03 +0200

    extension end point POST #1198

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/apiary/persistence/SalePersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/apiary/persistence/SalePersistenceMongodb.java
@@ -24,9 +24,11 @@ public class SalePersistenceMongodb implements SalePersistence {
 
     @Override
     public Sale create(Sale sale) {
+        SaleEntity saleEntity = new SaleEntity(sale);
         return this.saleRepository
-                .save(new SaleEntity(sale))
-                .toSale();    }
+                .save(saleEntity)
+                .toSale();
+    }
 
     @Override
     public boolean existIdSale(int idSale) {
