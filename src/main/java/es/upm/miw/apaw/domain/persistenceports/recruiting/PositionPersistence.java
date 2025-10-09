8711dd396beac17a0cb32f7ec7c8c290a5464e2d
jacardenas <juanancardenas@gmail.com>
2025-10-09 13:07:44 +0200

    added patch position recruiting #1205

--- src/main/java/es/upm/miw/apaw/domain/persistenceports/recruiting/PositionPersistence.java
+++ src/main/java/es/upm/miw/apaw/domain/persistenceports/recruiting/PositionPersistence.java
@@ -8,4 +8,7 @@ public interface PositionPersistence {
 
     Position create(Position position);
 
+    Position read(Integer reference);
+
+    void update(Integer reference, Position position);
 }
