af287a865e4587995e734e40fcd08ddaefee901e
DaniYDevs <danidzm18@gmail.com>
2025-10-10 01:15:21 +0200

    Added PATCH endpoint - StudentCouncil #1255

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/studentcouncil/entitites/RepresentativeEntity.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/studentcouncil/entitites/RepresentativeEntity.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw.adapters.mongodb.studentcouncil.entitites;
 
 import es.upm.miw.apaw.domain.models.UserDto;
+import es.upm.miw.apaw.domain.models.studentcouncil.Representative;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,4 +27,22 @@ public class RepresentativeEntity {
     private String responsibility;
     private UUID representativeId;
     private List<StudentIssueEntity> topics;
+
+    public Representative toRepresentative() {
+        return Representative.builder()
+                .joinDate(this.joinDate)
+                .responsibility(this.responsibility)
+                .representative(
+                        UserDto.builder()
+                                .id(this.representativeId)
+                                .build()
+                )
+                .topics(
+                        this.topics == null ? new ArrayList<>() :
+                                this.topics.stream()
+                                        .map(StudentIssueEntity::toStudentIssue)
+                                        .toList()
+                )
+                .build();
+    }
 }
--- src/main/java/es/upm/miw/apaw/adapters/mongodb/studentcouncil/entitites/StudentCouncilEntity.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/studentcouncil/entitites/StudentCouncilEntity.java
@@ -1,7 +1,9 @@
 package es.upm.miw.apaw.adapters.mongodb.studentcouncil.entitites;
 
+import es.upm.miw.apaw.domain.models.studentcouncil.StudentCouncil;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -11,6 +13,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 @Document
 public class StudentCouncilEntity {
@@ -22,5 +25,24 @@ public class StudentCouncilEntity {
     private String site;
     private BigDecimal resources;
 
+    @DBRef
     private List<RepresentativeEntity> representatives;
+
+
+    public StudentCouncil toStudentCouncil() {
+        return StudentCouncil.builder()
+                .id(this.id)
+                .council(this.council)
+                .site(this.site)
+                .resources(this.resources)
+                .representatives(this.representatives == null ? null :
+                        this.representatives.stream().map(RepresentativeEntity::toRepresentative).toList())
+                .build();
+    }
+
+    public void fromStudentCouncil(StudentCouncil studentCouncil) {
+        this.council = studentCouncil.getCouncil();
+        this.site = studentCouncil.getSite();
+        this.resources = studentCouncil.getResources();
+    }
 }
