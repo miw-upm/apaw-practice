ec79949759f48cd1220e41735124fd8bba2c12ae
jiangzhe00110 <jz2000110@gmail.com>
2025-10-09 19:21:23 +0200

    add Get endpoint for clothingstore#1244

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/clothingstore/daos/GarmentRepository.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/clothingstore/daos/GarmentRepository.java
@@ -2,7 +2,11 @@ package es.upm.miw.apaw.adapters.mongodb.clothingstore.daos;
 
 import es.upm.miw.apaw.adapters.mongodb.clothingstore.entities.GarmentEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
 public interface GarmentRepository extends MongoRepository<GarmentEntity, UUID> {
+    List<GarmentEntity> findByPriceBetween(BigDecimal min, BigDecimal max);
 }
