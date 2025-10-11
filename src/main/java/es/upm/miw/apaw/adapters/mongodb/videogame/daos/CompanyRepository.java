4c974a75ee3ab6f56f443727400f69a2deb45f96
SihuiXu <s.xu@alumnos.upm.es>
2025-10-11 02:06:32 +0200

    Endpoint POST Videogame #1262

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/videogame/daos/CompanyRepository.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/videogame/daos/CompanyRepository.java
@@ -3,7 +3,11 @@ package es.upm.miw.apaw.adapters.mongodb.videogame.daos;
 import es.upm.miw.apaw.adapters.mongodb.videogame.entities.CompanyEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CompanyRepository extends MongoRepository<CompanyEntity, UUID> {
+    Optional<CompanyEntity> findById(UUID id);
+
+    Optional<CompanyEntity> findByName(String name);
 }
