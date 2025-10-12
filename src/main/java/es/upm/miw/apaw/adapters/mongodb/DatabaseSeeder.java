47599c747d94225d0174045d79657614535f8f25
MinwangLou <minwang.lou@alumnos.upm.es>
2025-10-12 17:18:35 +0200

    added videoWebsite into DatabaseSeeder #1253

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/DatabaseSeeder.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/DatabaseSeeder.java
@@ -11,6 +11,7 @@ import es.upm.miw.apaw.adapters.mongodb.vehicle.daos.VehicleSeeder;
 import es.upm.miw.apaw.adapters.mongodb.apiary.daos.ApiarySeeder;
 import es.upm.miw.apaw.adapters.mongodb.university.daos.UniversitySeeder;
 import es.upm.miw.apaw.adapters.mongodb.recipes.daos.RecipesSeeder;
+import es.upm.miw.apaw.adapters.mongodb.videoWebsite.daos.VideoWebSiteSeeder;
 import es.upm.miw.apaw.adapters.mongodb.videogame.daos.VideogameSeeder;
 import es.upm.miw.apaw.adapters.mongodb.winery.daos.WinerySeeder;
 import es.upm.miw.apaw.adapters.mongodb.warehouse.daos.WarehouseSeeder;
@@ -42,6 +43,7 @@ public class DatabaseSeeder {
     private final FightersSeeder fightersSeeder;
     private final RecipesSeeder recipesSeeder;
     private final WarehouseSeeder warehouseSeeder;
+    private final VideoWebSiteSeeder videoWebSiteSeeder;
 
 
     @Autowired
@@ -60,7 +62,8 @@ public class DatabaseSeeder {
             FightersSeeder fightersSeeder,
             BankSeeder bankSeeder,
             RecipesSeeder recipesSeeder,
-            WarehouseSeeder warehouseSeeder
+            WarehouseSeeder warehouseSeeder,
+            VideoWebSiteSeeder videoWebsiteSeeder
             ) {
 
         this.shopSeeder = shopSeeder;
@@ -78,6 +81,7 @@ public class DatabaseSeeder {
         this.bankSeeder = bankSeeder;
         this.recipesSeeder = recipesSeeder;
         this.warehouseSeeder = warehouseSeeder;
+        this.videoWebSiteSeeder = videoWebsiteSeeder;
         this.seedDatabase();
 
     }
@@ -98,6 +102,7 @@ public class DatabaseSeeder {
         this.bankSeeder.seedDatabase();
         this.recipesSeeder.seedDatabase();
         this.warehouseSeeder.seedDatabase();
+        this.videoWebSiteSeeder.seedDatabase();
     }
 
     public void deleteAll() {
@@ -116,6 +121,7 @@ public class DatabaseSeeder {
         this.bankSeeder.deleteAll();
         this.recipesSeeder.deleteAll();
         this.warehouseSeeder.deleteAll();
+        this.videoWebSiteSeeder.deleteAll();
     }
 
     public void reSeedDatabase() {
