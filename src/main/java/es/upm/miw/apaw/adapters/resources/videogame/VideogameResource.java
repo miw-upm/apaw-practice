f67abb8b6ecc66f5bcd463db1b79b448e2adf8e8
SihuiXu <s.xu@alumnos.upm.es>
2025-10-14 21:07:06 +0200

    fix Test Endpoint DELETE Videogame #1290

--- src/main/java/es/upm/miw/apaw/adapters/resources/videogame/VideogameResource.java
+++ src/main/java/es/upm/miw/apaw/adapters/resources/videogame/VideogameResource.java
@@ -2,10 +2,7 @@ package es.upm.miw.apaw.adapters.resources.videogame;
 
 import es.upm.miw.apaw.domain.services.videogame.VideogameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(VideogameResource.VIDEOGAMES)
@@ -21,7 +18,7 @@ public class VideogameResource {
         this.videogameService = videogameService;
     }
 
-    @GetMapping(NAME_ID)
+    @DeleteMapping(NAME_ID)
     public void delete(@PathVariable String name){
         this.videogameService.delete(name);
     }
