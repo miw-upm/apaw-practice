1395ad6ddb1b05d3844993b4f2baa57cc8a4456c
Juliojld12 <119012467+Juliojld12@users.noreply.github.com>
2025-10-15 09:37:58 +0200

    story:football | test: verified persistence for club, player, stadium and match #1296

---
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/football/persistence/FootballPlayerPersistenceMongodb.java
@@ -0,0 +1,35 @@
+package es.upm.miw.apaw.adapters.mongodb.football.persistence;
+
+import es.upm.miw.apaw.adapters.mongodb.football.daos.FootballPlayerRepository;
+import es.upm.miw.apaw.adapters.mongodb.football.entities.FootballPlayerEntity;
+import es.upm.miw.apaw.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw.domain.models.football.FootballPlayer;
+import es.upm.miw.apaw.domain.persistenceports.football.FootballPlayerPersistence;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("footballPlayerPersistence")
+public class FootballPlayerPersistenceMongodb implements FootballPlayerPersistence {
+
+    private final FootballPlayerRepository playerRepository;
+    private static final String PLAYER_NICKNAME = "Football player nickname: ";
+
+    public FootballPlayerPersistenceMongodb(FootballPlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Override
+    public Optional<FootballPlayer> findByNickname(String nickname) {
+        return this.playerRepository.findByNickname(nickname)
+                .map(FootballPlayerEntity::toFootballPlayer);
+    }
+
+    @Override
+    public List<FootballPlayer> readAll() {
+        return this.playerRepository.findAll().stream()
+                .map(FootballPlayerEntity::toFootballPlayer)
+                .toList();
+    }
+}
