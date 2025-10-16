afdeaddd747e4590bcf65930db2bca967439400b
MinwangLou <minwang.lou@alumnos.upm.es>
2025-10-16 13:08:42 +0200

    Implemented Search 2 for VideoWebsite #1303

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/videoWebsite/entities/WebAccountEntity.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/videoWebsite/entities/WebAccountEntity.java
@@ -1,6 +1,7 @@
 package es.upm.miw.apaw.adapters.mongodb.videoWebsite.entities;
 
 import es.upm.miw.apaw.domain.models.UserDto;
+import es.upm.miw.apaw.domain.models.videoWebsite.Video;
 import es.upm.miw.apaw.domain.models.videoWebsite.WatchList;
 import es.upm.miw.apaw.domain.models.videoWebsite.WebAccount;
 import es.upm.miw.apaw.domain.models.videoWebsite.enums.AccountType;
@@ -31,20 +32,23 @@ public class WebAccountEntity {
 
     private UUID userId;
     private List<WatchListEntity> watchListEntities;
+    private List<VideoEntity> publishedVideoEntites;
 
     public WebAccountEntity(WebAccount webAccount){
-        BeanUtils.copyProperties(webAccount, this, "userId", "watchListEntities");
+        BeanUtils.copyProperties(webAccount, this, "userId", "watchListEntities", "publishedVideoEntites");
         this.userId = webAccount.getUser().getId();
         this.watchListEntities = webAccount.getWatchList().stream()
                 .map(WatchListEntity::new)
                 .toList();
-
+        this.publishedVideoEntites = webAccount.getPublishedVideos().stream()
+                .map(VideoEntity::new)
+                .toList();
     }
 
     public WebAccount toWebAccount() {
         WebAccount webAccount = new WebAccount();
 
-        BeanUtils.copyProperties(this, webAccount, "user", "userId", "watchListEntities", "watchList");
+        BeanUtils.copyProperties(this, webAccount, "user", "watchList", "publishedVideos");
         webAccount.setUser(UserDto.builder().id(this.userId).build());
 
         List<WatchList> watchLists = (this.watchListEntities == null)
@@ -54,6 +58,12 @@ public class WebAccountEntity {
                 .toList();
 
         webAccount.setWatchList(watchLists);
+        List<Video> publishedVideos = (this.publishedVideoEntites == null)
+                ?Collections.emptyList()
+                :this.publishedVideoEntites.stream()
+                .map(VideoEntity::toVideo)
+                .toList();
+        webAccount.setPublishedVideos(publishedVideos);
         return webAccount;
 
     }
