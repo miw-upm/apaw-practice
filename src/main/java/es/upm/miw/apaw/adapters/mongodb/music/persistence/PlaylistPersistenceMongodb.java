21799cd30d542033c1f7c4e29c60007ed70bdd25
raque <r.tricio@alumnos.upm.es>
2025-10-19 20:52:12 +0200

    implementation PUT endpoint #1325

--- src/main/java/es/upm/miw/apaw/adapters/mongodb/music/persistence/PlaylistPersistenceMongodb.java
+++ src/main/java/es/upm/miw/apaw/adapters/mongodb/music/persistence/PlaylistPersistenceMongodb.java
@@ -1,7 +1,9 @@
 package es.upm.miw.apaw.adapters.mongodb.music.persistence;
 
 import es.upm.miw.apaw.adapters.mongodb.music.daos.PlaylistRepository;
+import es.upm.miw.apaw.adapters.mongodb.music.entities.PlaylistEntity;
 import es.upm.miw.apaw.domain.exceptions.NotFoundException;
+import es.upm.miw.apaw.domain.models.music.Playlist;
 import es.upm.miw.apaw.domain.persistenceports.music.PlaylistPersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -21,5 +23,20 @@ public class PlaylistPersistenceMongodb implements PlaylistPersistence {
             throw new NotFoundException("Playlist not found: " + code);
         }
         this.playlistRepository.deleteById(code);
+    }
+
+    @Override
+    public void update(String code, Playlist playlist) {
+        PlaylistEntity entity = this.playlistRepository.findById(code)
+                .orElseThrow(() -> new NotFoundException("Playlist not found: " + code));
+
+        if (playlist.getLabel() != null) {
+            entity.setLabel(playlist.getLabel());
+        }
+        if (playlist.getOpened() != null) {
+            entity.setOpened(playlist.getOpened());
+        }
+
+        this.playlistRepository.save(entity);
     }
 }
