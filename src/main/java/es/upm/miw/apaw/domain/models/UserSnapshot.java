e56261ac0406d0dc5a006a53db7617647c073d5c
JBernal <j.bernal@upm.es>
2026-09-18 18:17:03 +0200

    clean models. miw-upm/apaw#14

--- src/main/java/es/upm/miw/apaw/domain/models/UserSnapshot.java
+++ src/main/java/es/upm/miw/apaw/domain/models/UserSnapshot.java
@@ -1,7 +1,5 @@
 package es.upm.miw.apaw.domain.models;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +13,6 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UserSnapshot {
     private UUID id;
-    @NotNull
-    @NotBlank
     private String mobile;
-    @NotNull
-    @NotBlank
     private String firstName;
 }
