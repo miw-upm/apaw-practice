23b478be19390525e01317735a1cf6ba3660eb02
wenyang-liu <wenyang@aethergenai.com>
2026-09-26 19:59:29 +0200

    add appointment persistence miw-upm/apaw#115

---
+++ src/main/java/es/upm/miw/apaw/adapters/out/appointment/postgres/AppointmentRepository.java
@@ -0,0 +1,10 @@
+package es.upm.miw.apaw.adapters.out.appointment.postgres;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface AppointmentRepository extends JpaRepository<AppointmentEntity, UUID>,
+        JpaSpecificationExecutor<AppointmentEntity> {
+}
---
+++ src/main/java/es/upm/miw/apaw/domain/ports/out/appointment/AppointmentGateway.java
@@ -0,0 +1,4 @@
+package es.upm.miw.apaw.domain.ports.out.appointment;
+
+public interface AppointmentGateway {
+}
